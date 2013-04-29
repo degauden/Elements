a2fd09ef9ea92d8ce6b30f292488ac0ea4abe77b
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-04-29 11:34:24 +0000

    removed the rpath settings for the install part. To be reviewed latter
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@2465 463500fe-d6be-4184-9b20-47717e75ecac

--- cmake/ElementsProjectConfig.cmake
+++ cmake/ElementsProjectConfig.cmake
@@ -29,23 +29,6 @@ set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_INCLUDE_DIRECTORIES_BEFORE ON)
 #set(CMAKE_SKIP_BUILD_RPATH TRUE)
 
-# when building, don't use the install RPATH already
-# (but later on when installing)
-SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-
-SET(CMAKE_INSTALL_RPATH "\$ORIGIN/../lib")
-
-# add the automatically determined parts of the RPATH
-# which point to directories outside the build tree to the install RPATH
-SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-
-
-# the RPATH to be used when installing, but only if it's not a system directory
-LIST(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
-IF("${isSystemDir}" STREQUAL "-1")
-   SET(CMAKE_INSTALL_RPATH "\$ORIGIN/../lib")
-ENDIF("${isSystemDir}" STREQUAL "-1")
-
 
 find_program(ccache_cmd ccache)
 find_program(distcc_cmd distcc)
