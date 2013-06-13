d47b57e71ef3f8feef699270d259e3a783d178dd
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-06-13 13:53:10 +0000

    Added raw implementation for real number comparison. One has been copied from the Google testing framework and the other one from http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm. An intermediate one using only function overloading with the Google technique must be crafted.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@2780 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ElementsKernel/src/Lib/Real.cpp
@@ -0,0 +1,57 @@
+/*
+ * Real.cpp
+ *
+ *  Created on: Jun 13, 2013
+ *      Author: hubert
+ */
+
+
+#include <cassert>
+#include <cmath>
+#include "ElementsKernel/Real.h"
+
+
+// Usable AlmostEqual function
+bool almostEqual2sComplement(float A, float B, int maxUlps=4)
+{
+    // Make sure maxUlps is non-negative and small enough that the
+    // default NAN won't compare as equal to anything.
+    assert(maxUlps > 0 && maxUlps < 4 * 1024 * 1024);
+
+    int aInt = *(int*)&A;
+    // Make aInt lexicographically ordered as a twos-complement int
+    if (aInt < 0)
+        aInt = 0x80000000 - aInt;
+    // Make bInt lexicographically ordered as a twos-complement int
+    int bInt = *(int*)&B;
+    if (bInt < 0)
+        bInt = 0x80000000 - bInt;
+    int intDiff = std::abs(aInt - bInt);
+    if (intDiff <= maxUlps)
+        return true;
+    return false;
+}
+
+
+bool almostEqual2sComplement(double A, double B, int maxUlps=10)
+{
+    // Make sure maxUlps is non-negative and small enough that the
+    // default NAN won't compare as equal to anything.
+    assert(maxUlps > 0 && maxUlps < 4 * 1024 * 1024);
+
+    long long aInt = *(long long*)&A;
+    // Make aInt lexicographically ordered as a twos-complement int
+    if (aInt < 0)
+        aInt = 0x8000000000000000 - aInt;
+
+    // Make bInt lexicographically ordered as a twos-complement int
+    long long bInt = *(long long*)&B;
+    if (bInt < 0)
+        bInt = 0x8000000000000000 - bInt;
+
+    long long intDiff = std::abs(aInt - bInt);
+    if (intDiff <= maxUlps && -maxUlps <= intDiff)
+    //if (intDiff <= maxUlps)
+        return true;
+    return false;
+}
