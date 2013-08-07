4b793e17ef45c7bddfd9928c1e741f84ff2308e3
Pierre Dubath <Pierre.Dubath@unige.ch>
2013-08-07 14:51:07 +0000

    The class ElementsProgram (.h and .cpp files) has been polished and properly documented. It should be ready for a code review.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3082 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/ElementsKernel/ElementsProgram.h
+++ ElementsKernel/ElementsKernel/ElementsProgram.h
@@ -1,10 +1,8 @@
 /**
  * @file ElementsProgram.h
- * 
- * Created on: Jul 18, 2013
- * 
- * @author Pierre Dubath
  *
+ * Created on: Aug 7, 2013
+ *     Author: Pierre Dubath
  */
 
 #ifndef ELEMENTSPROGRAM_H_
@@ -19,60 +17,170 @@
  * that derived from ElementsProgram, i.e., these derived classes
  * must end with the following line:
  * 		BUILD_MAIN_FOR(ElementsProgramExample)
+ *
+ * It also takes care of implementing the getVersion() method which
+ * returns the VERSION defined in the derived classes, with
+ * 		#define VERSION "Version svn: $Id$ ---- $Revision$
+ *
+ *	Note that svn expands the keywords ($Id$ ---- $Revision$) when
+ *	comitting code only if the svn client has been instructed with
+ * 		svn propset svn:keywords 'Id Revision' ElementsProgramExample.cpp
+ *
+ * 	TODO get release/tag version from svn rather than the Id Revision
+ * 	see Redmine Elements task 3240
+ *
+ * 	How to use this macro is shown in ElementsProgramExample.cpp
  */
-#define BUILD_MAIN_FOR(ElementsProgramName) 		\
-  int main(int argc, char** argv) 					\
-  {                               					\
+#define BUILD_MAIN_FOR(ElementsProgramName) 		    \
+  int main(int argc, char* argv[]) 					        \
+  {                               				        	\
     ElementsProgramName elementProgramInstance {} ;	\
-    return elementProgramInstance.run(argc, argv) ;	\
-  }
+    elementProgramInstance.run(argc, argv) ;	\
+  } 												                        \
+string ElementsProgramName::getVersion() {		    	\
+	return VERSION;									                  \
+}
 
 /**
  * @class ElementsProgram
  * @brief
- * 		Base class for all Element programs
+ * 		Base class for all Elements programs
  * @details
- * 		This base class takes care of program options and of logging
- * 		initialization. All Elements program extending this class
+ * 		This base class offers solutions for the common needs of
+ * 		all Elements programs, such as those dealing with program
+ * 		options and logging.
  */
 class ElementsProgram {
+
 public:
-	int run(int argc, char* argv[]);
+
+  /**
+   * @brief
+   *  This is the public entry point, i.e., the only method
+   *  called from the main
+   *
+   * @param argc
+   *   Command line argument number
+   * @param argv
+   *   Command line arguments
+   */
+  void run(int argc, char* argv[]);
 
 protected:
-	ElementsProgram();
-	virtual ~ElementsProgram();
 
-	/**
-	 * The pseudo main that all programs must implement.
-	 */
-	virtual int pseudoMain() = 0;
+  /**
+   * @brief Constructor
+   */
+  ElementsProgram() = default;
 
-	virtual boost::program_options::options_description defineSpecificProgramOptions() = 0;
+  /**
+   * @brief Destructor
+   */
+  virtual ~ElementsProgram() = default;
 
-	virtual std::string getVersion() = 0;
+  /**
+   * @brief
+   *   This methods must be used to define specific program options
+   * @details
+   *  This is the first method that must be implemented by all Elements programs
+   *  It is called from getProgramOptions(int argc,char* argv[])
+   *
+   * @return
+   *   A BOOST options description
+   */
+  virtual boost::program_options::options_description defineSpecificProgramOptions() = 0;
 
-	const boost::program_options::variables_map& getVariablesMap() const {
-		return m_variablesMap;
-	}
+  /**
+   * @brief
+   *   This is the "main" method os all Elements programs
+   * @details
+   *  This is the second method that must be implemented by all Elements programs
+   *  It is called from run(int argc,char* argv[])
+   */
+  virtual void pseudoMain() = 0;
+
+  /**
+   * @brief
+   *   Get the program version from svn tags
+   * @details
+   *   This method is implemented through the BUILD_MAIN_FOR(ElementsProgramName) macro,
+   *   therefore it should NOT be implemented in Elements programs
+   *
+   * @return
+   *   A string with the version information
+   */
+  virtual std::string getVersion() = 0;
+
+  /**
+   * @brief Getter
+   *
+   * @return
+   *   The BOOST variables_map which includes all program options.
+   */
+  const boost::program_options::variables_map& getVariablesMap() const {
+    return m_variablesMap;
+  }
 
 private:
 
-	const boost::filesystem::path getDefaultConfigFile(
-			std::string programName) const;
-	const boost::filesystem::path getDefaultLogFile(
-			std::string programName) const;
+  /**
+   * This is the BOOST program options variable_map used to store all
+   * program options. It is similar to a std::map but the element can be
+   * of different types. See the pseudoMain() in ElementsProgramExample.cpp
+   * to see how to retrieve options from this map.
+   */
+  boost::program_options::variables_map m_variablesMap { };
 
-	const boost::program_options::variables_map getProgramOptions(int argc,
-			char* argv[]);
+  /**
+   * @brief
+   *   Get a default configuration file name and path, to be used if not
+   *   provided as a command line option
+   *
+   * @return
+   *   A complete name/path to the default configuration file
+   */
+  const boost::filesystem::path getDefaultConfigFile(
+      std::string programName) const;
 
-	void logAllOptions(std::string programName);
+  /**
+   * @brief
+   *   Get a default log file name and path, to be used if not
+   *   provided as a command line option. By default a log file
+   *
+   *     ./"programName".log
+   *
+   *    will be returned
+   *
+   * @return
+   *   A complete name/path to the default log file
+   */
+  const boost::filesystem::path getDefaultLogFile(
+      std::string programName) const;
 
-	boost::filesystem::path m_logFile;
-	/**
-	 * This is the BOOST program options variable_map used to store all program options
-	 */
-	boost::program_options::variables_map m_variablesMap { };
+  /**
+   * @brief
+   *  Program setup taking care of command line options and logging
+   *  initialization
+   */
+  void setup(int argc, char* argv[]);
+
+  /**
+   * @brief
+   *  Get the program options from the command line into the
+   *  variables_map
+   *
+   *  @return
+   *    A BOOST variable_map
+   */
+  const boost::program_options::variables_map getProgramOptions(int argc,
+      char* argv[]);
+
+  /**
+   * @brief
+   *  Log all program options
+   *
+   */
+  void logAllOptions(std::string programName);
 
 };
 
