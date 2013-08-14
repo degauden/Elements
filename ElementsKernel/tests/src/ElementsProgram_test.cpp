cdd27425d303b80192fff598a6670d3f1fa150dd
Pierre Dubath <Pierre.Dubath@unige.ch>
2013-08-14 10:02:43 +0000

    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3120 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/tests/src/ElementsProgram_test.cpp
+++ ElementsKernel/tests/src/ElementsProgram_test.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <boost/test/unit_test.hpp>
 #include "ElementsKernel/ElementsException.h"
-#include "ElementsExamples/ClassExample.h"
+//#include "ElementsExamples/ClassExample.h"
 //#include "ElementsExamples/ElementsExamples/src/Program/ElementsProgramExample.cpp"
 using namespace std;
 
