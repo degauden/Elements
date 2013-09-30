dbb69fc393b9c8a90925c10a3f874d44693a2824
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-09-30 15:11:14 +0000

    fixed CCFits and Cfitsio cmake modules to properly detect the include path and link libraries
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/branches/cfitsio_dev@3317 463500fe-d6be-4184-9b20-47717e75ecac

--- cmake/modules/FindCCfits.cmake
+++ cmake/modules/FindCCfits.cmake
@@ -20,25 +20,19 @@
 #
 #   https://github.com/wangd/SciDB-HDF5/blob/master/cmake/Modules/FindCfitsio.cmake
 #
-
-include(FindPackageHandleStandardArgs)
-
-# Look for includes and libraries
-
-find_path(CCFITS_INCLUDE_DIR CCfits/CCfits
-  HINTS ${CCFITS_ROOT_DIR}
-  PATH_SUFFIXES include)
-
-find_library(CCFITS_LIBRARY CCfits
-  HINTS ${CCFITS_ROOT_DIR}
-  PATH_SUFFIXES lib)
-
-find_package_handle_standard_args(CCFITS DEFAULT_MSG
-  CCFITS_INCLUDE_DIR CCFITS_LIBRARY)
-
 if(NOT CCFITS_FOUND)
-  message("Can't find CCfits. You can specify a non-standard (e.g., from source) installation using CCFITS_ROOT_DIR")
-endif(NOT CCFITS_FOUND)
 
+  find_path(CCFITS_INCLUDE_DIR CCfits/CCfits)
 
-mark_as_advanced(CCFITS_LIBRARY CCFITS_INCLUDE_DIR)
+  find_library(CCFITS_LIBRARIES NAMES CCfits)
+
+  set(CCFITS_INCLUDE_DIRS ${CCFITS_INCLUDE_DIR})
+
+# handle the QUIETLY and REQUIRED arguments and set CPPUNIT_FOUND to TRUE if
+# all listed variables are TRUE
+  INCLUDE(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(CCfits DEFAULT_MSG CCFITS_INCLUDE_DIR CCFITS_LIBRARIES)
+
+  mark_as_advanced(CCFITS_FOUND CCFITS_INCLUDE_DIR CCFITS_LIBRARIES)
+
+endif()
