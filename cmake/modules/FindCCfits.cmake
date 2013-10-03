7ead7df82fa6c9883f7b2c420dfb1fbad71877c3
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-10-03 12:48:14 +0000

    reintegrated the cfitsio_dev branch
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3337 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ cmake/modules/FindCCfits.cmake
@@ -0,0 +1,38 @@
+#
+# FindCCfits.cmake
+#
+# Created on: Aug 19, 2013
+#     Author: Pavel Binko
+#
+# -- Check for CCfits library --
+#
+# Defines:
+#
+#  CCFITS_FOUND
+#  CCFITS_INCLUDE
+#  CCFITS_LIBRARY
+#
+# Hints for non-standard locations:
+#
+#   CCFITS_ROOT_DIR (used for finding CCfits)
+#
+# Source of information :
+#
+#   https://github.com/wangd/SciDB-HDF5/blob/master/cmake/Modules/FindCfitsio.cmake
+#
+if(NOT CCFITS_FOUND)
+
+  find_path(CCFITS_INCLUDE_DIR CCfits/CCfits)
+
+  find_library(CCFITS_LIBRARIES NAMES CCfits)
+
+  set(CCFITS_INCLUDE_DIRS ${CCFITS_INCLUDE_DIR})
+
+# handle the QUIETLY and REQUIRED arguments and set CPPUNIT_FOUND to TRUE if
+# all listed variables are TRUE
+  INCLUDE(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(CCfits DEFAULT_MSG CCFITS_INCLUDE_DIR CCFITS_LIBRARIES)
+
+  mark_as_advanced(CCFITS_FOUND CCFITS_INCLUDE_DIR CCFITS_LIBRARIES)
+
+endif()
