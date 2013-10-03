6251ebca2fd135922b303db9c23d0c7ae25f820c
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2013-10-03 12:48:14 +0000

    reintegrated the cfitsio_dev branch
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3337 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ cmake/modules/FindCfitsio.cmake
@@ -0,0 +1,39 @@
+#
+# FindCfitsio.cmake
+#
+# Created on: Aug 19, 2013
+#     Author: Pavel Binko
+#
+# -- Check for CFITSIO library --
+#
+# Defines:
+#
+#  CFITSIO_FOUND - system has the CFITSIO library
+#  CFITSIO_INCLUDE_DIR - the CFITSIO include directory
+#  CFITSIO_LIBRARY - The library needed to use CFITSIO
+#
+# Hints for non-standard locations:
+#
+#   CFITSIO_ROOT_DIR (used for finding cfitsio)
+#
+# Source of information :
+#
+#   https://github.com/wangd/SciDB-HDF5/blob/master/cmake/Modules/FindCfitsio.cmake
+#
+
+if(NOT CFITSIO_FOUND)
+
+  find_path(CFITSIO_INCLUDE_DIR fitsio.h PATH_SUFFIXES cfitsio)
+
+  find_library(CFITSIO_LIBRARIES NAMES cfitsio)
+
+  set(CFITSIO_INCLUDE_DIRS ${CFITSIO_INCLUDE_DIR})
+
+# handle the QUIETLY and REQUIRED arguments and set CFITSIO_FOUND to TRUE if
+# all listed variables are TRUE
+  INCLUDE(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Cfitsio DEFAULT_MSG CFITSIO_INCLUDE_DIR CFITSIO_LIBRARIES)
+
+  mark_as_advanced(CFITSIO_FOUND CFITSIO_INCLUDE_DIR CFITSIO_LIBRARIES)
+
+endif()
