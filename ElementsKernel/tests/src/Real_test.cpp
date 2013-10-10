e64b69531f69d43102dd5707a7a381cda6c12767
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-10-10 10:31:25 +0000

    Added the isEqual function to compare floating point numbers
    - remove the useless Real namespace
    - added tests for isEqual for both default max_ulps and custom ones.
    
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3372 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/tests/src/Real_test.cpp
+++ ElementsKernel/tests/src/Real_test.cpp
@@ -12,7 +12,6 @@
 
 
 using namespace std;
-using namespace Real;
 
 //-----------------------------------------------------------------------------
 
@@ -81,6 +80,111 @@ BOOST_FIXTURE_TEST_CASE( DoubleCompare_test, RealFix ) {
 
 
 } // Eof DoubleCompare_test
+
+BOOST_FIXTURE_TEST_CASE( FloatCompare2_test, RealFix ) {
+
+	float f = 1.0F/2.0F ;
+
+	BOOST_CHECK_EQUAL(f, 0.5F);
+
+	BOOST_CHECK( ! isEqual(f, 0.5100000F));
+	BOOST_CHECK( ! isEqual(f, 0.5010000F));
+	BOOST_CHECK( ! isEqual(f, 0.5001000F));
+	BOOST_CHECK( ! isEqual(f, 0.5000100F));
+	BOOST_CHECK( ! isEqual(f, 0.5000010F));
+	BOOST_CHECK( ! isEqual(f, 0.5000003F));
+
+	BOOST_CHECK(   isEqual(f, 0.5000002F));
+	BOOST_CHECK(   isEqual(f, 0.5000001F));
+	BOOST_CHECK(   isEqual(f, 0.5F));
+
+
+} // Eof FloatCompare2_test
+
+BOOST_FIXTURE_TEST_CASE( DoubleCompare2_test, RealFix ) {
+
+	double f = 1.0/2.0 ;
+
+	BOOST_CHECK_EQUAL(f, 0.5);
+
+	BOOST_CHECK( ! isEqual(f, 0.510000000000000));
+	BOOST_CHECK( ! isEqual(f, 0.501000000000000));
+	BOOST_CHECK( ! isEqual(f, 0.500100000000000));
+	BOOST_CHECK( ! isEqual(f, 0.500010000000000));
+	BOOST_CHECK( ! isEqual(f, 0.500001000000000));
+	BOOST_CHECK( ! isEqual(f, 0.500000100000000));
+	BOOST_CHECK( ! isEqual(f, 0.500000010000000));
+	BOOST_CHECK( ! isEqual(f, 0.500000001000000));
+	BOOST_CHECK( ! isEqual(f, 0.500000000100000));
+	BOOST_CHECK( ! isEqual(f, 0.500000000010000));
+	BOOST_CHECK( ! isEqual(f, 0.500000000001000));
+	BOOST_CHECK( ! isEqual(f, 0.500000000000100));
+	BOOST_CHECK( ! isEqual(f, 0.500000000000010));
+	BOOST_CHECK( ! isEqual(f, 0.500000000000005));
+	BOOST_CHECK( ! isEqual(f, 0.500000000000004));
+	BOOST_CHECK( ! isEqual(f, 0.500000000000003));
+	BOOST_CHECK( ! isEqual(f, 0.500000000000002));
+
+	BOOST_CHECK(   isEqual(f, 0.500000000000001));
+	BOOST_CHECK(   isEqual(f, 0.5));
+
+
+} // Eof DoubleCompare2_test
+
+BOOST_FIXTURE_TEST_CASE( FloatCompare3_test, RealFix ) {
+
+	float f = 1.0F/2.0F ;
+
+	BOOST_CHECK_EQUAL(f, 0.5F);
+
+	BOOST_CHECK(( ! isEqual<float,14>(f, 0.5100000F)));
+	BOOST_CHECK(( ! isEqual<float,14>(f, 0.5010000F)));
+	BOOST_CHECK(( ! isEqual<float,14>(f, 0.5001000F)));
+	BOOST_CHECK(( ! isEqual<float,14>(f, 0.5000100F)));
+	BOOST_CHECK(( ! isEqual<float,14>(f, 0.5000010F)));
+
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000008F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000007F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000006F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000005F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000004F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000003F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000002F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5000001F)));
+	BOOST_CHECK((   isEqual<float,14>(f, 0.5F)));
+
+
+} // Eof FloatCompare3_test
+
+BOOST_FIXTURE_TEST_CASE( DoubleCompare3_test, RealFix ) {
+
+	double f = 1.0/2.0 ;
+
+	BOOST_CHECK_EQUAL(f, 0.5);
+
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.510000000000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.501000000000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500100000000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500010000000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500001000000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000100000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000010000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000001000000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000100000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000010000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000001000)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000000100)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000000010)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000000005)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000000004)));
+	BOOST_CHECK(( ! isEqual<double,20>(f, 0.500000000000003)));
+
+	BOOST_CHECK((   isEqual<double,20>(f, 0.500000000000002)));
+	BOOST_CHECK((   isEqual<double,20>(f, 0.500000000000001)));
+	BOOST_CHECK((   isEqual<double,20>(f, 0.5)));
+
+
+} // Eof DoubleCompare3_test
 
 //-----------------------------------------------------------------------------
 // End of the Boost tests
