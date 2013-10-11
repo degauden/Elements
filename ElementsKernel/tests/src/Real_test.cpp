8c0d3a257f06406b54203c0b58a42cfb3b72ae33
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-10-11 11:23:39 +0000

    Added isNotEqual function for completeness.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3381 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/tests/src/Real_test.cpp
+++ ElementsKernel/tests/src/Real_test.cpp
@@ -242,6 +242,198 @@ BOOST_FIXTURE_TEST_CASE( DoubleCompare4_test, RealFix ) {
 } // Eof DoubleCompare4_test
 
 
+
+
+
+
+
+
+
+
+
+
+
+BOOST_FIXTURE_TEST_CASE( FloatCompare5_test, RealFix ) {
+
+  float f = 1.0F/2.0F ;
+
+  BOOST_CHECK_EQUAL(f, 0.5F);
+
+  BOOST_CHECK(   isNotEqual(f, 0.5100000F));
+  BOOST_CHECK(   isNotEqual(f, 0.5010000F));
+  BOOST_CHECK(   isNotEqual(f, 0.5001000F));
+  BOOST_CHECK(   isNotEqual(f, 0.5000100F));
+  BOOST_CHECK(   isNotEqual(f, 0.5000010F));
+  BOOST_CHECK(   isNotEqual(f, 0.5000003F));
+
+  BOOST_CHECK( ! isNotEqual(f, 0.5000002F));
+  BOOST_CHECK( ! isNotEqual(f, 0.5000001F));
+  BOOST_CHECK( ! isNotEqual(f, 0.5F));
+
+
+} // Eof FloatCompare5_test
+
+BOOST_FIXTURE_TEST_CASE( DoubleCompare5_test, RealFix ) {
+
+  double f = 1.0/2.0 ;
+
+  BOOST_CHECK_EQUAL(f, 0.5);
+
+  BOOST_CHECK(   isNotEqual(f, 0.510000000000000));
+  BOOST_CHECK(   isNotEqual(f, 0.501000000000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500100000000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500010000000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500001000000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000100000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000010000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000001000000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000100000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000010000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000001000));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000000100));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000000010));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000000005));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000000004));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000000003));
+  BOOST_CHECK(   isNotEqual(f, 0.500000000000002));
+
+  BOOST_CHECK( ! isNotEqual(f, 0.500000000000001));
+  BOOST_CHECK( ! isNotEqual(f, 0.5));
+
+
+} // Eof DoubleCompare5_test
+
+BOOST_FIXTURE_TEST_CASE( FloatCompare6_test, RealFix ) {
+
+  float f = 1.0F/2.0F ;
+
+  BOOST_CHECK_EQUAL(f, 0.5F);
+
+  BOOST_CHECK((   isNotEqual<float,14>(f, 0.5100000F)));
+  BOOST_CHECK((   isNotEqual<float,14>(f, 0.5010000F)));
+  BOOST_CHECK((   isNotEqual<float,14>(f, 0.5001000F)));
+  BOOST_CHECK((   isNotEqual<float,14>(f, 0.5000100F)));
+  BOOST_CHECK((   isNotEqual<float,14>(f, 0.5000010F)));
+
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000008F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000007F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000006F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000005F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000004F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000003F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000002F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5000001F)));
+  BOOST_CHECK(( ! isNotEqual<float,14>(f, 0.5F)));
+
+
+} // Eof FloatCompare6_test
+
+BOOST_FIXTURE_TEST_CASE( DoubleCompare6_test, RealFix ) {
+
+  double f = 1.0/2.0 ;
+
+  BOOST_CHECK_EQUAL(f, 0.5);
+
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.510000000000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.501000000000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500100000000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500010000000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500001000000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000100000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000010000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000001000000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000100000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000010000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000001000)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000000100)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000000010)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000000005)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000000004)));
+  BOOST_CHECK((   isNotEqual<double,20>(f, 0.500000000000003)));
+
+  BOOST_CHECK(( ! isNotEqual<double,20>(f, 0.500000000000002)));
+  BOOST_CHECK(( ! isNotEqual<double,20>(f, 0.500000000000001)));
+  BOOST_CHECK(( ! isNotEqual<double,20>(f, 0.5)));
+
+
+} // Eof DoubleCompare6_test
+
+BOOST_FIXTURE_TEST_CASE( FloatCompare7_test, RealFix ) {
+
+  float f = 1.0F/2.0F ;
+
+  BOOST_CHECK_EQUAL(f, 0.5F);
+
+  BOOST_CHECK((   isNotEqual<14>(f, 0.5100000F)));
+  BOOST_CHECK((   isNotEqual<14>(f, 0.5010000F)));
+  BOOST_CHECK((   isNotEqual<14>(f, 0.5001000F)));
+  BOOST_CHECK((   isNotEqual<14>(f, 0.5000100F)));
+  BOOST_CHECK((   isNotEqual<14>(f, 0.5000010F)));
+
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000008F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000007F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000006F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000005F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000004F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000003F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000002F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5000001F)));
+  BOOST_CHECK(( ! isNotEqual<14>(f, 0.5F)));
+
+
+} // Eof FloatCompare7_test
+
+BOOST_FIXTURE_TEST_CASE( DoubleCompare7_test, RealFix ) {
+
+  double f = 1.0/2.0 ;
+
+  BOOST_CHECK_EQUAL(f, 0.5);
+
+  BOOST_CHECK((   isNotEqual<20>(f, 0.510000000000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.501000000000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500100000000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500010000000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500001000000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000100000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000010000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000001000000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000100000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000010000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000001000)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000000100)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000000010)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000000005)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000000004)));
+  BOOST_CHECK((   isNotEqual<20>(f, 0.500000000000003)));
+
+  BOOST_CHECK(( ! isNotEqual<20>(f, 0.500000000000002)));
+  BOOST_CHECK(( ! isNotEqual<20>(f, 0.500000000000001)));
+  BOOST_CHECK(( ! isNotEqual<20>(f, 0.5)));
+
+
+} // Eof DoubleCompare7_test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //-----------------------------------------------------------------------------
 // End of the Boost tests
 BOOST_AUTO_TEST_SUITE_END ()
