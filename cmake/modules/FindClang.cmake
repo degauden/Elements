4bc223d2aea45baf1f5a977dc9478b317baf706e
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2013-11-06 11:50:40 +0000

    Added global support for the Clang (LLVM) compiler. This will probably have to be modified for the MacOSX clang version.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3489 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ cmake/modules/FindClang.cmake
@@ -0,0 +1,20 @@
+find_package(LLVM)
+
+find_program(CLANG_C_COMPILER
+             NAMES clang
+         	 DOC "Clang C Compiler")
+         	 
+find_program(CLANG_CXX_COMPILER
+             NAMES clang++
+         	 DOC "Clang C++ Compiler")
+         	 
+# handle the QUIETLY and REQUIRED arguments and set ODB_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CLANG  DEFAULT_MSG
+                                  CLANG_C_COMPILER CLANG_CXX_COMPILER)
+#
+IF(CLANG_FOUND)
+    MESSAGE(STATUS "Found Clang C Compiler (CLANG_C_COMPILER = ${CLANG_C_COMPILER})")
+    MESSAGE(STATUS "Found Clang C++ Compiler (CLANG_CXX_COMPILER = ${CLANG_CXX_COMPILER})")
+ENDIF()
---
+++ cmake/modules/FindLLVM.cmake
@@ -0,0 +1,34 @@
+
+find_program(LLVM_AR
+             NAMES llvm-ar
+         	 DOC "LLVM ar")
+         	 
+find_program(LLVM_LINKER
+             NAMES llvm-link
+         	 DOC "LLVM linker")
+
+find_program(LLVM_NM
+             NAMES llvm-nm
+         	 DOC "LLVM nm")
+
+find_program(LLVM_OBJDUMP
+             NAMES llvm-objdump
+         	 DOC "LLVM objdump")
+
+find_program(LLVM_RANLIB
+             NAMES llvm-ranlib
+         	 DOC "LLVM ranlib")
+
+# handle the QUIETLY and REQUIRED arguments and set ODB_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LLVM  DEFAULT_MSG
+                                  LLVM_AR LLVM_LINKER LLVM_NM LLVM_OBJDUMP LLVM_RANLIB)
+#
+IF(LLVM_FOUND)
+    MESSAGE(STATUS "Found LLVM ar (LLVM_AR = ${LLVM_AR})")
+    MESSAGE(STATUS "Found LLVM linker (LLVM_LINKER = ${LLVM_LINKER})")
+    MESSAGE(STATUS "Found LLVM nm (LLVM_NM = ${LLVM_NM})")
+    MESSAGE(STATUS "Found LLVM objdump (LLVM_OBJDUMP = ${LLVM_OBJDUMP})")
+    MESSAGE(STATUS "Found LLVM ranlib (LLVM_RANLIB = ${LLVM_RANLIB})")
+ENDIF()
