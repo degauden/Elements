4c73c7c033e8099d9741d73768bd10ddb3988ace
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2013-12-02 09:33:03 +0000

    fixed wrong warnings from cppcheck
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3640 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/src/Lib/Real.cpp
+++ ElementsKernel/src/Lib/Real.cpp
@@ -22,12 +22,14 @@ bool almostEqual2sComplement(const float& a, const float& b, const int& max_ulps
     assert(max_ulps > 0 && max_ulps < 4 * 1024 * 1024);
 
 //    int a_int = *(int*)&a;
+    // cppcheck-suppress invalidPointerCast
     int a_int = *reinterpret_cast<const int *>(&a);
     // Make a_int lexicographically ordered as a twos-complement int
     if (a_int < 0)
         a_int = 0x80000000 - a_int;
     // Make b_int lexicographically ordered as a twos-complement int
     //    int b_int = *(int*)&b;
+    // cppcheck-suppress invalidPointerCast
     int b_int = *reinterpret_cast<const int *>(&b);
     if (b_int < 0)
         b_int = 0x80000000 - b_int;
@@ -45,6 +47,7 @@ bool almostEqual2sComplement(const double& a, const double& b, const int& max_ul
     assert(max_ulps > 0 && max_ulps < 4 * 1024 * 1024);
 
 //    long long a_int = *(long long*)&a;
+    // cppcheck-suppress invalidPointerCast
     long long a_int = *reinterpret_cast<const long long *>(&a);
     // Make a_int lexicographically ordered as a twos-complement int
     if (a_int < 0)
@@ -52,6 +55,7 @@ bool almostEqual2sComplement(const double& a, const double& b, const int& max_ul
 
     // Make b_int lexicographically ordered as a twos-complement int
 //    long long b_int = *(long long*)&b;
+    // cppcheck-suppress invalidPointerCast
     long long b_int = *reinterpret_cast<const long long*>(&b);
     if (b_int < 0)
         b_int = 0x8000000000000000 - b_int;
