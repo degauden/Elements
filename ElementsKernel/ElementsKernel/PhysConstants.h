199ad9be3b4ac3d29b4877cd301ac75a93101008
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2013-12-18 12:44:53 +0000

    added the files for common definition of units, math and physics constants.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3713 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ElementsKernel/ElementsKernel/PhysConstants.h
@@ -0,0 +1,75 @@
+/*
+ * PhysConstants.h
+ *
+ *  Created on: Dec 18, 2013
+ *      Author: hubert
+ */
+
+#ifndef ELEMENTSKERNEL_PHYSCONSTANTS_H_
+#define ELEMENTSKERNEL_PHYSCONSTANTS_H_
+
+#include "ElementsKernel/SystemOfUnits.h"
+
+namespace Elements {
+  namespace Units {
+
+    static const double Avogadro = 6.0221367e+23/mole;
+
+    //
+    // c   = 299.792458 mm/ns
+    // c^2 = 898.7404 (mm/ns)^2
+    //
+    static const double c_light   = 2.99792458e+8 * m/s;
+    static const double c_squared = c_light * c_light;
+
+    //
+    // h     = 4.13566e-12 MeV*ns
+    // hbar  = 6.58212e-13 MeV*ns
+    // hbarc = 197.32705e-12 MeV*mm
+    //
+    static const double h_Planck      = 6.62606957e-34 * joule * s;
+    static const double hbar_Planck   = h_Planck/twopi;
+    static const double hbarc         = hbar_Planck * c_light;
+    static const double hbarc_squared = hbarc * hbarc;
+
+    static const double electron_charge = - eplus; // see SystemOfUnits.h
+    static const double e_squared = eplus * eplus;
+
+    //
+    // amu_c2 - atomic equivalent mass unit
+    // amu    - atomic mass unit
+    //
+    static const double electron_mass = 9.10938291e-31 * kg;
+    static const double   proton_mass = 1.672621777e-27 * kg;
+    static const double           amu = 1.660538921e-27 * kg;
+
+    //
+    // permeability of free space mu0    = 2.01334e-16 Mev*(ns*eplus)^2/mm
+    // permittivity of free space epsil0 = 5.52636e+10 eplus^2/(MeV*mm)
+    //
+    static const double mu0      = 4*pi*1.e-7 * henry/m;
+    static const double epsilon0 = 1./(c_squared*mu0);
+
+    //
+    // electromagnetic coupling = 1.43996e-12 MeV*mm/(eplus^2)
+    //
+    static const double elm_coupling           = e_squared/(4*pi*epsilon0);
+    static const double fine_structure_const   = elm_coupling/hbarc;
+
+
+    static const double k_Boltzmann = 1.3806488e-23 * joule/kelvin ;
+
+    static const double STP_Temperature = 273.15*kelvin;
+    static const double STP_Pressure    = 1.*atmosphere;
+    static const double kGasThreshold   = 10.*mg/cm3;
+
+    static const double universe_mean_density = 1.e-25*g/cm3;
+
+  }  // namespace Units
+} // namespace Elements
+
+
+
+
+
+#endif /* ELEMENTSKERNEL_PHYSCONSTANTS_H_ */
