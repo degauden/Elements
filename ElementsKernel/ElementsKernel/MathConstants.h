2e8e91a2238feb91c3b7cdd4055bf26c96e18cfa
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-12-18 12:44:53 +0000

    added the files for common definition of units, math and physics constants.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3713 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ElementsKernel/ElementsKernel/MathConstants.h
@@ -0,0 +1,26 @@
+/*
+ * MathConstants.h
+ *
+ *  Created on: Dec 18, 2013
+ *      Author: hubert
+ */
+
+#ifndef ELEMENTSKERNEL_MATHCONSTANTS_H_
+#define ELELEMTSKERNEL_MATHCONSTANTS_H_
+
+namespace Elements {
+namespace Units {
+
+static const double     pi  = 3.14159265358979323846;
+static const double  twopi  = 2.0*pi;
+static const double halfpi  = pi/2.0;
+static const double     pi2 = pi*pi;
+
+
+} // namespace Constants
+} // namespace Elements
+
+
+
+
+#endif /* ELELMENTSKERNEL_MATHCONSTANTS_H_ */
