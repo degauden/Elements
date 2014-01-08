ff6a71e05f043c04a883a5f1dd22d44a4d987d5c
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-01-08 16:41:07 +0000

    fixed file doxygen comment
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@3769 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/ElementsKernel/PhysConstants.h
+++ ElementsKernel/ElementsKernel/PhysConstants.h
@@ -21,38 +21,33 @@ namespace Elements {
     /// @brief \f$c^2\f$
     static const double c_squared = c_light * c_light;
 
-    //
-    // h     = 4.13566e-12 MeV*ns
-    // hbar  = 6.58212e-13 MeV*ns
-    // hbarc = 197.32705e-12 MeV*mm
-    //
+    /// @brief \f$ h \f$
     static const double h_Planck      = 6.62606957e-34 * joule * s;
+    /// @brief \f$ \hbar=h/2\pi\f$
     static const double hbar_Planck   = h_Planck/twopi;
+    /// @brief \f$ \hbar c\f$
     static const double hbarc         = hbar_Planck * c_light;
+    /// @brief \f$ (\hbar c)^2\f$
     static const double hbarc_squared = hbarc * hbarc;
 
+    /// @brief \f$ -e \f$
     static const double electron_charge = - eplus; // see SystemOfUnits.h
+    /// @brief \f$ e^2 \f$
     static const double e_squared = eplus * eplus;
 
-    //
-    // amu_c2 - atomic equivalent mass unit
-    // amu    - atomic mass unit
-    //
+    /// @brief \f$ m_e \f$
     static const double electron_mass = 9.10938291e-31 * kg;
+    /// @brief \f$ m_p \f$
     static const double   proton_mass = 1.672621777e-27 * kg;
     static const double           amu = 1.660538921e-27 * kg;
 
-    //
-    // permeability of free space mu0    = 2.01334e-16 Mev*(ns*eplus)^2/mm
-    // permittivity of free space epsil0 = 5.52636e+10 eplus^2/(MeV*mm)
-    //
+    /// @brief permeability of free space \f$\mu_0\f$
     static const double mu0      = 4*pi*1.e-7 * henry/m;
+    /// @brief permeability of free space \f$\epsilon_0=1/(c^2\mu_0)\f$
     static const double epsilon0 = 1./(c_squared*mu0);
 
-    //
-    // electromagnetic coupling = 1.43996e-12 MeV*mm/(eplus^2)
-    //
     static const double elm_coupling           = e_squared/(4*pi*epsilon0);
+    /// @brief \f$ \alpha=e^2/(4\pi\epsilon_0\hbar c)\f$
     static const double fine_structure_const   = elm_coupling/hbarc;
 
 
