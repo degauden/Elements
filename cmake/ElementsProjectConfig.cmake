4340c4a01d43af063133081a62c477ef07c81ca3
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-04-16 15:00:04 +0200

    add the targz make target
    This target replaces the creation of the source tarball file that was
    done (wrongly) at configure time.

--- cmake/ElementsProjectConfig.cmake
+++ cmake/ElementsProjectConfig.cmake
@@ -648,10 +648,15 @@ macro(elements_project project version)
 
   include(CPack)
 
-  message(STATUS "Creating the source tarball: ${PROJECT_BINARY_DIR}/_CPack_Packages/${BINARY_TAG}/RPM/SOURCES/${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}.tar.gz")
-  file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/_CPack_Packages/${BINARY_TAG}/RPM/SOURCES)
-  execute_process(COMMAND tar zcf ${PROJECT_BINARY_DIR}/_CPack_Packages/${BINARY_TAG}/RPM/SOURCES/${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}.tar.gz --exclude "build.*" --exclude "./.*" --exclude "./InstallArea" --transform "s/./${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}/"  .
-                  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+  find_package(Tar)
+  if(TAR_FOUND)
+    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/_CPack_Packages/${BINARY_TAG}/RPM/SOURCES)
+    add_custom_target(targz
+                      COMMAND ${TAR_EXECUTABLE} zcf ${PROJECT_BINARY_DIR}/_CPack_Packages/${BINARY_TAG}/RPM/SOURCES/${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}.tar.gz --exclude "build.*" --exclude "./.*" --exclude "./InstallArea" --transform "s/./${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}/"  .
+                      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                      COMMENT "Generating The Source TarBall ${PROJECT_BINARY_DIR}/_CPack_Packages/${BINARY_TAG}/RPM/SOURCES/${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}.tar.gz" VERBATIM
+    )  
+  endif()
 
   # Add Doxygen generation
   find_package(Doxygen)
