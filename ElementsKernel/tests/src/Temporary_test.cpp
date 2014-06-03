5b2d32167e1dc0401965d35204604461dd192362
Hubert Degaudenzi <Hubert.Degaudenzi@gmail.com>
2014-06-03 11:03:51 +0200

    Test the destruction of a non-empty temporary directory

--- ElementsKernel/tests/src/Temporary_test.cpp
+++ ElementsKernel/tests/src/Temporary_test.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 
 namespace fs = boost::filesystem;
 
@@ -40,23 +41,43 @@ BOOST_AUTO_TEST_SUITE (Temporary_test)
 
 BOOST_FIXTURE_TEST_CASE(AutoDestruct_test, Temporary_Fixture) {
 
+  // handle on created path names
   fs::path test_path;
   fs::path test_file_path ;
 
   {
-	TempDir One;
-	test_path = One.path();
+	// block creation for local variables
+	TempDir one;
+	test_path = one.path();
 	BOOST_CHECK(fs::exists(test_path));
 
-	TempFile Two;
-	test_file_path = Two.path();
+	TempFile two;
+	test_file_path = two.path();
 	BOOST_CHECK(fs::exists(test_file_path));
   }
 
+  // the items must have been destroy after the
+  // closing of the block
   BOOST_CHECK(!fs::exists(test_path));
   BOOST_CHECK(!fs::exists(test_file_path));
 
-  BOOST_CHECK(true);
+  fs::path test2_path ;
+  fs::path test2_file_path ;
+
+  {
+	TempDir three;
+	test2_path = three.path();
+	test2_file_path = test2_path / "toto.txt" ;
+	BOOST_CHECK(!fs::exists(test2_file_path)) ;
+	fs::ofstream ofs(test2_file_path);
+	ofs << "test text" << endl;
+	ofs.close();
+	BOOST_CHECK(fs::exists(test2_file_path)) ;
+	BOOST_CHECK(fs::exists(test2_path)) ;
+  }
+
+  BOOST_CHECK(!fs::exists(test2_path)) ;
+  BOOST_CHECK(!fs::exists(test2_file_path)) ;
 
 }
 
