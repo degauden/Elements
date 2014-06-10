bd4deb6c300734c1e6d0d2f18e867a00d4ddec9d
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-06-10 13:24:40 +0200

    remove useless WIN32 preprocessor branching

--- ElementsKernel/src/Lib/ModuleInfo.cpp
+++ ElementsKernel/src/Lib/ModuleInfo.cpp
@@ -6,31 +6,14 @@
 #include "ElementsKernel/ModuleInfo.h"
 #include "ElementsKernel/System.h"
 
-#ifdef _WIN32
-  #define NOMSG
-  #define NOGDI
-  #  define strcasecmp _stricmp
-  #  define strncasecmp _strnicmp
-  #include "process.h"
-  #include "windows.h"
-  #include "Win32PsApi.h"
-static PsApiFunctions _psApi;
-  #define getpid _getpid
-  #undef NOMSG
-  #undef NOGDI
-  #ifndef PATH_MAX
-  #  define PATH_MAX 1024
-  #endif
-#else  // UNIX...: first the EGCS stuff, then the OS dependent includes
-  #include <errno.h>
-  #include <string.h>
-  #include "sys/times.h"
-  #include "sys/param.h"
-  #include "unistd.h"
-  #include "libgen.h"
-  #include <cstdio>
-  #include <dlfcn.h>
-#endif
+#include <errno.h>
+#include <string.h>
+#include "sys/times.h"
+#include "sys/param.h"
+#include "unistd.h"
+#include "libgen.h"
+#include <cstdio>
+#include <dlfcn.h>
 
 using namespace std;
 
@@ -42,16 +25,7 @@ const string& Elements::System::moduleName()   {
   static string module("");
   if ( module == "" )   {
     if ( processHandle() && moduleHandle() )    {
-#ifdef _WIN32
-      char moduleName[256] = {"Unknown.module"};
-      moduleName[0] = 0;
-      if ( _psApi.isValid() )   {
-        _psApi.GetModuleBaseNameA( processHandle(), (HINSTANCE)moduleHandle(), moduleName, sizeof(moduleName) );
-      }
-      string mod = moduleName;
-#elif defined(__linux) || defined(__APPLE__)
       string mod = ::basename((char*)((Dl_info*)moduleHandle())->dli_fname);
-#endif
       module = mod.substr(static_cast<string::size_type>(0), mod.rfind('.'));
     }
   }
@@ -65,19 +39,12 @@ const string& Elements::System::moduleNameFull()   {
     if ( processHandle() && moduleHandle() )    {
       char name[PATH_MAX] = {"Unknown.module"};
       name[0] = 0;
-#ifdef _WIN32
-      if ( _psApi.isValid() )   {
-        _psApi.GetModuleFileNameExA( processHandle(), (HINSTANCE)moduleHandle(), name,sizeof(name) );
-        module = name;
-      }
-#else
       const char *path =
 #  if defined(__linux) || defined(__APPLE__)
           ((Dl_info*)moduleHandle())->dli_fname;
 #  endif
       if (::realpath(path, name))
         module = name;
-#endif
     }
   }
   return module;
@@ -93,11 +60,7 @@ Elements::System::ModuleType Elements::System::moduleType()   {
       type = EXECUTABLE;
     else if ( module[loc] == 'e' || module[loc] == 'E' )
       type = EXECUTABLE;
-#ifdef _WIN32
-    else if ( module[loc] == 'd' || module[loc] == 'D' )
-#else
     else if ( module[loc] == 's' && module[loc+1] == 'o' )
-#endif
       type = SHAREDLIB;
     else
       type = UNKNOWN;
@@ -108,11 +71,7 @@ Elements::System::ModuleType Elements::System::moduleType()   {
 /// Retrieve processhandle
 void* Elements::System::processHandle()   {
   static long pid = ::getpid();
-#ifdef _WIN32
-  static HANDLE hP = ::OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ,FALSE,pid);
-#else
   static void* hP = (void*)pid;
-#endif
   return hP;
 }
 
@@ -123,15 +82,6 @@ void Elements::System::setModuleHandle(Elements::System::ImageHandle handle)    
 Elements::System::ImageHandle Elements::System::moduleHandle()    {
   if ( 0 == ModuleHandle )    {
     if ( processHandle() )    {
-#ifdef _WIN32
-      static HINSTANCE handle = 0;
-      DWORD   cbNeeded;
-      if ( 0 == handle && _psApi.isValid() )    {
-        if ( _psApi.EnumProcessModules( processHandle(), &handle, sizeof(ModuleHandle), &cbNeeded) )   {
-        }
-      }
-      return handle;
-#elif defined(__linux) || defined(__APPLE__)
       static Dl_info info;
       if ( 0 !=
            ::dladdr(
@@ -143,25 +93,12 @@ Elements::System::ImageHandle Elements::System::moduleHandle()    {
                , &info) ) {
 	return &info;
       }
-#endif
     }
   }
   return ModuleHandle;
 }
 
 Elements::System::ImageHandle Elements::System::exeHandle()    {
-#ifdef _WIN32
-  if ( processHandle() )    {
-    static HINSTANCE handle = 0;
-    DWORD   cbNeeded;
-    if ( 0 == handle && _psApi.isValid() )    {
-      if ( _psApi.EnumProcessModules( processHandle(), &handle, sizeof(ModuleHandle), &cbNeeded) )   {
-      }
-    }
-    return handle;
-  }
-  return 0;
-#elif defined(__linux) || defined(__APPLE__)
   // This does NOT work!
   static Dl_info infoBuf, *info = &infoBuf;
   if ( 0 == info ) {
@@ -179,7 +116,6 @@ Elements::System::ImageHandle Elements::System::exeHandle()    {
     }
   }
   return info;
-#endif
 }
 
 const string& Elements::System::exeName()    {
@@ -187,38 +123,17 @@ const string& Elements::System::exeName()    {
   if ( module.length() == 0 )    {
     char name[PATH_MAX] = {"Unknown.module"};
     name[0] = 0;
-#ifdef _WIN32
-    if ( _psApi.isValid() && processHandle() )   {
-      _psApi.GetModuleFileNameExA( processHandle(), (HINSTANCE)exeHandle(), name,sizeof(name) );
-      module = name;
-    }
-#elif defined(__linux) || defined(__APPLE__)
     char cmd[512];
     ::sprintf(cmd, "/proc/%d/exe", ::getpid());
     module = "Unknown";
     if (::readlink(cmd, name, sizeof(name)) >= 0)
       module = name;
-#endif
   }
   return module;
 }
 
 const vector<string> Elements::System::linkedModules()    {
   if ( s_linkedModules.size() == 0 )    {
-#ifdef _WIN32
-    DWORD  cbNeeded;
-    HINSTANCE handle[1024];
-    if ( _psApi.isValid() )    {
-      if ( _psApi.EnumProcessModules(processHandle(),handle,sizeof(handle),&cbNeeded) )   {
-        for (size_t i = 0; i < cbNeeded/sizeof(HANDLE); i++ )    {
-          char   name[255];  // Maximum file name length on NT 4.0
-          if ( 0 < _psApi.GetModuleFileNameExA( processHandle(), handle[i], name, sizeof(name)) )   {
-            s_linkedModules.push_back(name);
-          }
-        }
-      }
-    }
-#elif defined(__linux) || defined(__APPLE__)
     char ff[512], cmd[1024], fname[1024], buf1[64], buf2[64], buf3[64], buf4[64];
     ::sprintf(ff, "/proc/%d/maps", ::getpid());
     FILE* maps = ::fopen(ff, "r");
@@ -230,7 +145,6 @@ const vector<string> Elements::System::linkedModules()    {
       }
     }
     ::fclose(maps);
-#endif
   }
   return s_linkedModules;
 }
