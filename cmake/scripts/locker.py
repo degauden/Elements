cb896a4d8f5d8b87f90f369e5847dca399dbaa95
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-08-06 11:04:05 +0200

    Reformat the code
    - fixes of PyDev warnings
    - application of autopep8 formatting

--- cmake/scripts/locker.py
+++ cmake/scripts/locker.py
@@ -1,14 +1,16 @@
+#@PydevCodeAnalysisIgnore
 ##########################################################
-## stolen and (slighty) adapted from:
-##  http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/65203
+# stolen and (slighty) adapted from:
+# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/65203
 ##
 
-import os, time
+import os
+import time
 
 if os.name == 'nt':
     import msvcrt
 elif os.name == 'posix':
-    import fcntl
+    import fcntl  # @UnresolvedImport
     LOCK_EX = fcntl.F_WRLCK
     LOCK_SH = fcntl.F_RDLCK
     LOCK_NB = fcntl.F_UNLCK
@@ -16,73 +18,80 @@ else:
     raise RuntimeError("Locker only defined for nt and posix platforms")
 
 if os.name == 'nt':
-    def lock(file):
+    def lock(filename):
         """
-        Lock first 10 bytes of a file.
-        """ 
-        pos = file.tell() # remember current position
-        file.seek(0)
+        Lock first 10 bytes of a filename.
+        """
+        pos = filename.tell()  # remember current position
+        filename.seek(0)
         # By default, python tries about 10 times, then throws an exception.
         # We want to wait forever.
         acquired = False
         while not acquired:
             try:
-                msvcrt.locking(file.fileno(),msvcrt.LK_LOCK,10)
+                msvcrt.locking(filename.fileno(), msvcrt.LK_LOCK, 10)
                 acquired = True
             except IOError, x:
-                if x.errno != 36: # 36, AKA 'Resource deadlock avoided', is normal
+                # 36, AKA 'Resource deadlock avoided', is normal
+                if x.errno != 36:
                     raise
-        file.seek(pos) # reset position
+        filename.seek(pos)  # reset position
 
-    def unlock(file):
+    def unlock(filename):
         """
-        Unlock first 10 bytes of a file.
-        """ 
-        pos = file.tell() # remember current position
-        file.seek(0)
-        msvcrt.locking(file.fileno(),msvcrt.LK_UNLCK,10)
-        file.seek(pos) # reset position
+        Unlock first 10 bytes of a filename.
+        """
+        pos = filename.tell()  # remember current position
+        filename.seek(0)
+        msvcrt.locking(filename.fileno(), msvcrt.LK_UNLCK, 10)
+        filename.seek(pos)  # reset position
 
-elif os.name =='posix':
-    import socket, errno
+elif os.name == 'posix':
+    import socket
+    import errno
 
     def _tmpFileName(fileName):
-        return "%s.%s.%d" % ( fileName, socket.gethostname(), os.getpid() )
+        return "%s.%s.%d" % (fileName, socket.gethostname(), os.getpid())
+
     def _lckFileName(fileName):
         return "%s.lock" % fileName
 
-    def _linkCount( lockFileName ):
+    def _linkCount(lockFileName):
         try:
             return os.stat(lockFileName).st_nlink
         except OSError, e:
             if e.errno != errno.ENOENT:
                 raise
             return -1
+
     def _read(fileName):
         try:
             fp = open(fileName)
-            try:     readFileName = fp.read()
-            finally: fp.close()
+            try:
+                readFileName = fp.read()
+            finally:
+                fp.close()
             return readFileName
         except EnvironmentError, e:
             if e.errno != errno.ENOENT:
                 raise
             return None
-            
+
     def _sleep(): time.sleep(8)
 
-    def lock(file):
-        fileName = file.name
+    def lock(filename):
+        fileName = filename.name
         tmpFileName = _tmpFileName(fileName)
-        fp = open( tmpFileName, "w" )
-        fp.write( tmpFileName )
+        fp = open(tmpFileName, "w")
+        fp.write(tmpFileName)
         fp.close()
 
-        lockFileName = _lckFileName( fileName )
+        lockFileName = _lckFileName(fileName)
         while True:
-            if _linkCount(lockFileName) != -1: _sleep()
+            if _linkCount(lockFileName) != -1:
+                _sleep()
             try:
-                os.link( tmpFileName, lockFileName )
+                os.link(tmpFileName, lockFileName)
                 # we acquired the lock
                 return
             except OSError, e:
@@ -91,60 +100,66 @@ elif os.name =='posix':
                 elif e.errno != errno.EEXIST:
                     os.unlink(tmpFileName)
                     raise
-                elif _linkCount( lockFileName ) != 2:
+                elif _linkCount(lockFileName) != 2:
                     pass
                 elif _read(lockFileName) == tmpFileName:
                     raise
                 else:
                     # someone else owns that lock
                     pass
-            ## didn't get the lock !!
-            ## say something ?
-            #print _id_()," failed to acquire the lock ..."
+            # didn't get the lock !!
+            # say something ?
+            # print _id_()," failed to acquire the lock ..."
             _sleep()
             pass
         return
-    
-    def unlock(file):
-        fileName = file.name
-        tmpFileName  = _tmpFileName(fileName)
-        lockFileName = _lckFileName( fileName )
+
+    def unlock(filename):
+        fileName = filename.name
+        tmpFileName = _tmpFileName(fileName)
+        lockFileName = _lckFileName(fileName)
 
         try:
-            os.unlink( lockFileName )
+            os.unlink(lockFileName)
         except OSError, e:
             if e.errno != errno.ENOENT:
                 raise
-        # remove the tmp file
+        # remove the tmp filename
         try:
-            os.unlink( tmpFileName )
+            os.unlink(tmpFileName)
         except OSError, e:
             if e.errno != errno.ENOENT:
                 raise
         return
 
 import logging
-## Lock a file.
+# Lock a file.
 #  The file for the lock is created if it doesn't exists and it the "temporary"
 #  argument is set to True it will also be deleted when the lock is not needed.
 #  The unlocking is done in the destructor (RAII pattern).
+
+
 class LockFile(object):
-    def __init__(self, name, temporary = False):
+
+    def __init__(self, name, temporary=False):
         self.name = name
         self.temporary = temporary
         self.file = None
         self.log = logging.getLogger("LockFile")
-        self.log.info("%s - Locking on %s", time.strftime("%Y-%m-%d_%H:%M:%S"), self.name)
+        self.log.info(
+            "%s - Locking on %s", time.strftime("%Y-%m-%d_%H:%M:%S"), self.name)
         if not os.path.exists(name):
             mode = "w"
         else:
-            self.temporary = False # I do not want to delete a file I didn't create
+            # I do not want to delete a file I didn't create
+            self.temporary = False
             mode = "r+"
         try:
             self.file = open(self.name, mode)
             lock(self.file)
         except:
             self.log.warning("Cannot acquire lock on %s", self.name)
+
     def __del__(self):
         if self.file:
             unlock(self.file)
@@ -154,5 +169,5 @@ class LockFile(object):
                     os.remove(self.name)
                 except:
                     pass
-            self.log.info("%s - Lock on %s released", time.strftime("%Y-%m-%d_%H:%M:%S"), self.name)
-
+            self.log.info(
+                "%s - Lock on %s released", time.strftime("%Y-%m-%d_%H:%M:%S"), self.name)
