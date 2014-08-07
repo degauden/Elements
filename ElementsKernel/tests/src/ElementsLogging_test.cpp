937fe3272b56a6b1fabe595bb3ffb2b8b25a8130
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-08-07 14:53:14 +0200

    Fix the member naming in the test fixtures

--- ElementsKernel/tests/src/ElementsLogging_test.cpp
+++ ElementsKernel/tests/src/ElementsLogging_test.cpp
@@ -1,4 +1,4 @@
-/** 
+/**
  * @file ElementsLogging_test.cpp
  * @date January 14, 2014
  * @author Nikolaos Apostolakos
@@ -71,8 +71,8 @@ struct ElementsLogging_Fixture {
   }
   // This tracker will record all messages written in the stderr. The Elements
   // logging system guarantees that the messages will appear there.
-  LogMessageTracker tracker {std::cerr};
-  ElementsLogging logger = ElementsLogging::getLogger("TestLogger");
+  LogMessageTracker m_tracker {std::cerr};
+  ElementsLogging m_logger = ElementsLogging::getLogger("TestLogger");
 };
 
 class SetRandomSeed {
@@ -93,16 +93,16 @@ BOOST_AUTO_TEST_SUITE (ElementsLogging_test)
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(loggerNames_test, ElementsLogging_Fixture) {
-  
+
   //Given
   ElementsLogging logger2 = ElementsLogging::getLogger("TestLogger2");
-  
+
   // When
-  logger.info("From logger 1");
+  m_logger.info("From logger 1");
   logger2.info("From logger 2");
-  
+
   // Then
-  auto messages = tracker.getMessages();
+  auto messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 2);
   std::string name1;
   std::tie(std::ignore, std::ignore, name1, std::ignore) = messages[0];
@@ -118,29 +118,29 @@ BOOST_FIXTURE_TEST_CASE(loggerNames_test, ElementsLogging_Fixture) {
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(messageTextAndLevel_test, ElementsLogging_Fixture) {
-  
+
   // Given
-  logger.setLevel(ElementsLogging::LoggingLevel::DEBUG);
-  
+  m_logger.setLevel(ElementsLogging::LoggingLevel::DEBUG);
+
   // When
-  logger.debug("Debug message");
-  logger.info("Info message");
-  logger.warn("Warn message");
-  logger.error("Error message");
-  logger.fatal("Fatal message");
-  logger.debug("Debug message with %d value", 15);
-  logger.info("Info message with %d value", 15);
-  logger.warn("Warn message with %d value", 15);
-  logger.error("Error message with %d value", 15);
-  logger.fatal("Fatal message with %d value", 15);
-  logger.debug() << "Debug message with " << 15 << " value";
-  logger.info() << "Info message with " << 15 << " value";
-  logger.warn() << "Warn message with " << 15 << " value";
-  logger.error() << "Error message with " << 15 << " value";
-  logger.fatal() << "Fatal message with " << 15 << " value";
-  
+  m_logger.debug("Debug message");
+  m_logger.info("Info message");
+  m_logger.warn("Warn message");
+  m_logger.error("Error message");
+  m_logger.fatal("Fatal message");
+  m_logger.debug("Debug message with %d value", 15);
+  m_logger.info("Info message with %d value", 15);
+  m_logger.warn("Warn message with %d value", 15);
+  m_logger.error("Error message with %d value", 15);
+  m_logger.fatal("Fatal message with %d value", 15);
+  m_logger.debug() << "Debug message with " << 15 << " value";
+  m_logger.info() << "Info message with " << 15 << " value";
+  m_logger.warn() << "Warn message with " << 15 << " value";
+  m_logger.error() << "Error message with " << 15 << " value";
+  m_logger.fatal() << "Fatal message with " << 15 << " value";
+
   // Then
-  auto messages = tracker.getMessages();
+  auto messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 15);
   ElementsLogging::LoggingLevel logLevel;
   std::string message;
@@ -197,79 +197,79 @@ BOOST_FIXTURE_TEST_CASE(messageTextAndLevel_test, ElementsLogging_Fixture) {
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(setLevel_test, ElementsLogging_Fixture) {
-  
+
   // Given
-  logger.setLevel(ElementsLogging::LoggingLevel::DEBUG);
-  
+  m_logger.setLevel(ElementsLogging::LoggingLevel::DEBUG);
+
   // When
-  logger.debug("Debug message");
-  logger.info("Info message");
-  logger.warn("Warn message");
-  logger.error("Error message");
-  logger.fatal("Fatal message");
-  
+  m_logger.debug("Debug message");
+  m_logger.info("Info message");
+  m_logger.warn("Warn message");
+  m_logger.error("Error message");
+  m_logger.fatal("Fatal message");
+
   // Then
-  auto messages = tracker.getMessages();
+  auto messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 5);
-  
+
   // Given
-  tracker.reset();
-  logger.setLevel(ElementsLogging::LoggingLevel::INFO);
-  
+  m_tracker.reset();
+  m_logger.setLevel(ElementsLogging::LoggingLevel::INFO);
+
   // When
-  logger.debug("Debug message");
-  logger.info("Info message");
-  logger.warn("Warn message");
-  logger.error("Error message");
-  logger.fatal("Fatal message");
-  
+  m_logger.debug("Debug message");
+  m_logger.info("Info message");
+  m_logger.warn("Warn message");
+  m_logger.error("Error message");
+  m_logger.fatal("Fatal message");
+
   // Then
-  messages = tracker.getMessages();
+  messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 4);
-  
+
   // Given
-  tracker.reset();
-  logger.setLevel(ElementsLogging::LoggingLevel::WARN);
-  
+  m_tracker.reset();
+  m_logger.setLevel(ElementsLogging::LoggingLevel::WARN);
+
   // When
-  logger.debug("Debug message");
-  logger.info("Info message");
-  logger.warn("Warn message");
-  logger.error("Error message");
-  logger.fatal("Fatal message");
-  
+  m_logger.debug("Debug message");
+  m_logger.info("Info message");
+  m_logger.warn("Warn message");
+  m_logger.error("Error message");
+  m_logger.fatal("Fatal message");
+
   // Then
-  messages = tracker.getMessages();
+  messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 3);
-  
+
   // Given
-  tracker.reset();
-  logger.setLevel(ElementsLogging::LoggingLevel::ERROR);
-  
+  m_tracker.reset();
+  m_logger.setLevel(ElementsLogging::LoggingLevel::ERROR);
+
   // When
-  logger.debug("Debug message");
-  logger.info("Info message");
-  logger.warn("Warn message");
-  logger.error("Error message");
-  logger.fatal("Fatal message");
-  
+  m_logger.debug("Debug message");
+  m_logger.info("Info message");
+  m_logger.warn("Warn message");
+  m_logger.error("Error message");
+  m_logger.fatal("Fatal message");
+
   // Then
-  messages = tracker.getMessages();
+  messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 2);
-  
+
   // Given
-  tracker.reset();
-  logger.setLevel(ElementsLogging::LoggingLevel::FATAL);
-  
+  m_tracker.reset();
+  m_logger.setLevel(ElementsLogging::LoggingLevel::FATAL);
+
   // When
-  logger.debug("Debug message");
-  logger.info("Info message");
-  logger.warn("Warn message");
-  logger.error("Error message");
-  logger.fatal("Fatal message");
-  
+  m_logger.debug("Debug message");
+  m_logger.info("Info message");
+  m_logger.warn("Warn message");
+  m_logger.error("Error message");
+  m_logger.fatal("Fatal message");
+
   // Then
-  messages = tracker.getMessages();
+  messages = m_tracker.getMessages();
   BOOST_CHECK_EQUAL(messages.size(), 1);
 }
 
@@ -278,16 +278,16 @@ BOOST_FIXTURE_TEST_CASE(setLevel_test, ElementsLogging_Fixture) {
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(setLogFile_test, ElementsLogging_Fixture) {
-  
+
   // Given
   std::stringstream logFileName {};
   logFileName << "/tmp/" << std::time(NULL) << std::rand() << ".log";
   ElementsLogging::setLogFile(logFileName.str());
-  
+
   // When
-  logger.error("First message");
-  logger.info("Second message");
-  
+  m_logger.error("First message");
+  m_logger.info("Second message");
+
   // Then
   BOOST_CHECK(boost::filesystem::exists(logFileName.str()));
   std::ifstream logFile {logFileName.str()};
@@ -307,21 +307,21 @@ BOOST_FIXTURE_TEST_CASE(setLogFile_test, ElementsLogging_Fixture) {
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(singleLogFile_test, ElementsLogging_Fixture) {
-  
+
   // Given
   std::stringstream logFileName1 {};
   logFileName1 << "/tmp/" << std::time(NULL) << std::rand() << ".log";
   std::stringstream logFileName2 {};
   logFileName2 << "/tmp/" << std::time(NULL) << std::rand() << ".log";
-  
+
   // When
-  
+
   ElementsLogging::setLogFile(logFileName1.str());
-  logger.error("First message");
+  m_logger.error("First message");
   ElementsLogging::setLogFile(logFileName2.str());
-  logger.info("Second message");
-  logger.info("Third message");
-  
+  m_logger.info("Second message");
+  m_logger.info("Third message");
+
   // Then
   BOOST_CHECK(boost::filesystem::exists(logFileName1.str()));
   std::ifstream logFile1 {logFileName1.str()};
--- ElementsKernel/tests/src/PathSearch_test.cpp
+++ ElementsKernel/tests/src/PathSearch_test.cpp
@@ -28,18 +28,18 @@ using namespace std;
 
 struct Path_Fixture {
 
-  string env_variable_name = "ELEMENTS_CONF_PATH";
+  string m_env_variable_name = "ELEMENTS_CONF_PATH";
   // store the original value of the environment variable to set it again correctly after the test
-  const char * tmp_char_ptr = ::getenv(env_variable_name.c_str());
+  const char * m_tmp_char_ptr = ::getenv(m_env_variable_name.c_str());
 
-  string root_path = ::getenv("ELEMENTSKERNELROOT");
-  string string_full_path = root_path + "/tests/data/PathSearch/";
-  fs::path full_path { string_full_path };
+  string m_root_path = ::getenv("ELEMENTSKERNELROOT");
+  string m_string_full_path = m_root_path + "/tests/data/PathSearch/";
+  fs::path m_full_path { m_string_full_path };
 
-  string full_path_1 = root_path
+  string m_full_path_1 = m_root_path
       + "/tests/data/PathSearch/dirForTestingRecursiveSearch";
-  string full_path_2 = root_path + "/tests/data/PathSearch//ElementsKernel";
-  string multiple_path = "/opt/local/bin:" + full_path_1 + ":" + full_path_2
+  string m_full_path_2 = m_root_path + "/tests/data/PathSearch//ElementsKernel";
+  string m_multiple_path = "/opt/local/bin:" + m_full_path_1 + ":" + m_full_path_2
       + ":/bin";
 
   Path_Fixture() {
@@ -47,8 +47,8 @@ struct Path_Fixture {
   }
   ~Path_Fixture() {
     // teardown
-    if (tmp_char_ptr != nullptr) {
-      ::setenv(env_variable_name.c_str(), tmp_char_ptr, 1);
+    if (m_tmp_char_ptr != nullptr) {
+      ::setenv(m_env_variable_name.c_str(), m_tmp_char_ptr, 1);
     }
   }
 };
@@ -61,11 +61,11 @@ BOOST_FIXTURE_TEST_CASE(simple_file_search, Path_Fixture) {
 
   string file_name { "MockFile_up.conf" };
 
-  fs::path expectedFullPath = full_path / fs::path(file_name);
+  fs::path expectedFullPath = m_full_path / fs::path(file_name);
   fs::path actualFullPath =
-      pathSearch(file_name, full_path, SearchType::Local).at(0);
+      pathSearch(file_name, m_full_path, SearchType::Local).at(0);
   BOOST_CHECK(actualFullPath == expectedFullPath);
-  actualFullPath = pathSearch(file_name, full_path, SearchType::Recursive).at(
+  actualFullPath = pathSearch(file_name, m_full_path, SearchType::Recursive).at(
       0);
   BOOST_CHECK(actualFullPath == expectedFullPath);
 
@@ -75,12 +75,12 @@ BOOST_FIXTURE_TEST_CASE(simple_directory_search, Path_Fixture) {
 
   string file_name { "ElementsKernel" };
 
-  fs::path expectedFullPath = full_path / fs::path(file_name);
+  fs::path expectedFullPath = m_full_path / fs::path(file_name);
   fs::path actualFullPath =
-      pathSearch(file_name, full_path, SearchType::Local).at(0);
+      pathSearch(file_name, m_full_path, SearchType::Local).at(0);
   BOOST_CHECK(actualFullPath == expectedFullPath);
   BOOST_CHECK(
-      pathSearch(file_name, full_path, SearchType::Recursive).size() == 2);
+      pathSearch(file_name, m_full_path, SearchType::Recursive).size() == 2);
 
 }
 
@@ -89,12 +89,12 @@ BOOST_FIXTURE_TEST_CASE(recursive_file_search, Path_Fixture) {
   string file_name { "MockFile_down.conf" };
 
   // should not be found on a Local search, pathSearch returns a vector of size = 0
-  BOOST_CHECK(pathSearch(file_name, full_path, SearchType::Local).size() == 0);
+  BOOST_CHECK(pathSearch(file_name, m_full_path, SearchType::Local).size() == 0);
 
-  fs::path expectedFullPath = full_path
+  fs::path expectedFullPath = m_full_path
       / fs::path("dirForTestingRecursiveSearch") / fs::path(file_name);
   // only one MockFile_down.conf in first position of the path vector
-  fs::path actualFullPath = pathSearch(file_name, full_path,
+  fs::path actualFullPath = pathSearch(file_name, m_full_path,
       SearchType::Recursive).at(0);
   BOOST_CHECK(actualFullPath == expectedFullPath);
 }
@@ -105,12 +105,12 @@ BOOST_FIXTURE_TEST_CASE(recursive_search_string_interface, Path_Fixture) {
 
   // should not be found on a Local search, pathSearch returns a vector of size = 0
   BOOST_CHECK(
-      pathSearch(file_name, full_path.string(), SearchType::Local).size() == 0);
+      pathSearch(file_name, m_full_path.string(), SearchType::Local).size() == 0);
 
-  fs::path expectedFullPath = full_path
+  fs::path expectedFullPath = m_full_path
       / fs::path("dirForTestingRecursiveSearch") / fs::path(file_name);
   // only one MockFile_down.conf in first position of the path vector
-  string actualFullPath = pathSearch(file_name, full_path.string(),
+  string actualFullPath = pathSearch(file_name, m_full_path.string(),
       SearchType::Recursive).at(0);
   BOOST_CHECK(actualFullPath == expectedFullPath.string());
 }
@@ -120,11 +120,11 @@ BOOST_FIXTURE_TEST_CASE(duplicate_file_search, Path_Fixture) {
   string file_name { "MockFile_replicate.conf" };
 
   // should not be found on a Local search, pathSearch returns a vector of size = 0
-  BOOST_CHECK(pathSearch(file_name, full_path, SearchType::Local).size() == 0);
+  BOOST_CHECK(pathSearch(file_name, m_full_path, SearchType::Local).size() == 0);
 
   // two MockFile_replicate.conf should be found on a recursive search
   BOOST_CHECK(
-      pathSearch(file_name, full_path.string(), SearchType::Recursive).size()
+      pathSearch(file_name, m_full_path.string(), SearchType::Recursive).size()
           == 2);
 
 }
@@ -133,15 +133,15 @@ BOOST_FIXTURE_TEST_CASE(duplicate_file_search, Path_Fixture) {
 
 BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_single_file, Path_Fixture) {
 
-  setenv(env_variable_name.c_str(), multiple_path.c_str(), 1);
+  setenv(m_env_variable_name.c_str(), m_multiple_path.c_str(), 1);
   string file { "MockFile_in_ElementsKernel_up.conf" };
 
   vector<fs::path> actualFullPathVector = pathSearchInEnvVariable(file,
-      env_variable_name);
+      m_env_variable_name);
 
   BOOST_CHECK(actualFullPathVector.size() == 1);
 
-  fs::path expectedFullPath = full_path / fs::path {
+  fs::path expectedFullPath = m_full_path / fs::path {
       "ElementsKernel/MockFile_in_ElementsKernel_up.conf" };
 
   BOOST_CHECK(expectedFullPath == actualFullPathVector.at(0));
@@ -149,17 +149,17 @@ BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_single_file, Path_Fixture) {
 
 BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_multiple_file, Path_Fixture) {
 
-  setenv(env_variable_name.c_str(), multiple_path.c_str(), 1);
+  setenv(m_env_variable_name.c_str(), m_multiple_path.c_str(), 1);
   string file { "MockFile_replicate.conf" };
 
   vector<fs::path> actualFullPathVector = pathSearchInEnvVariable(file,
-      env_variable_name);
+      m_env_variable_name);
 
   BOOST_CHECK(actualFullPathVector.size() == 2);
 
-  fs::path expectedFullPath_1 = full_path_1 / fs::path {
+  fs::path expectedFullPath_1 = m_full_path_1 / fs::path {
       "ElementsKernel/MockFile_replicate.conf" };
-  fs::path expectedFullPath_2 = full_path_2 / fs::path {
+  fs::path expectedFullPath_2 = m_full_path_2 / fs::path {
       "MockFile_replicate.conf" };
 
   if (expectedFullPath_1 != actualFullPathVector.at(0)) {
@@ -178,47 +178,47 @@ BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_multiple_file, Path_Fixture) {
 BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_order, Path_Fixture) {
 
   // first order
-  string multiple_path = "/opt/local/bin:" + full_path_1 + ":" + full_path_2
+  string multiple_path = "/opt/local/bin:" + m_full_path_1 + ":" + m_full_path_2
       + ":/bin";
-  setenv(env_variable_name.c_str(), multiple_path.c_str(), 1);
+  setenv(m_env_variable_name.c_str(), multiple_path.c_str(), 1);
   string file_1 { "MockFile_replicate.conf" };
 
   vector<fs::path> actualFullPathVector = pathSearchInEnvVariable(file_1,
-      env_variable_name);
-  fs::path expectedFullPath = full_path_1 / fs::path {
+      m_env_variable_name);
+  fs::path expectedFullPath = m_full_path_1 / fs::path {
       "ElementsKernel/MockFile_replicate.conf" };
   BOOST_CHECK(expectedFullPath == actualFullPathVector.at(0));
 
   // Opposite order
-  multiple_path = "/opt/local/bin:" + full_path_2 + ":" + full_path_1 + ":/bin";
-  setenv(env_variable_name.c_str(), multiple_path.c_str(), 1);
+  multiple_path = "/opt/local/bin:" + m_full_path_2 + ":" + m_full_path_1 + ":/bin";
+  setenv(m_env_variable_name.c_str(), multiple_path.c_str(), 1);
   string file_2 { "MockFile_replicate.conf" };
 
-  actualFullPathVector = pathSearchInEnvVariable(file_2, env_variable_name);
-  expectedFullPath = full_path_2 / fs::path { "MockFile_replicate.conf" };
+  actualFullPathVector = pathSearchInEnvVariable(file_2, m_env_variable_name);
+  expectedFullPath = m_full_path_2 / fs::path { "MockFile_replicate.conf" };
   BOOST_CHECK(expectedFullPath == actualFullPathVector.at(0));
 }
 
 BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_file_not_found, Path_Fixture) {
 
-  setenv(env_variable_name.c_str(), multiple_path.c_str(), 1);
+  setenv(m_env_variable_name.c_str(), m_multiple_path.c_str(), 1);
   string file { "NonExistentFile.conf" };
 
   vector<fs::path> actualFullPathVector = pathSearchInEnvVariable(file,
-      env_variable_name);
+      m_env_variable_name);
 
   BOOST_CHECK(actualFullPathVector.size() == 0);
 }
 
 BOOST_FIXTURE_TEST_CASE(searchFileInEnvVariable_Env_Variable_Undefine, Path_Fixture) {
 
-  unsetenv(env_variable_name.c_str());
+  unsetenv(m_env_variable_name.c_str());
   string file { "MockFile_replicate.conf" };
 
   bool exception = false;
    try {
      vector<fs::path> actualFullPathVector = pathSearchInEnvVariable(file,
-          env_variable_name);
+          m_env_variable_name);
 
    } catch (const ElementsException & e) {
      string exception_str = e.what();
