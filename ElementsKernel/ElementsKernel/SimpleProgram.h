b6e70909d77e78b3865fd69422f4e099d7d5ec72
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-08-27 12:09:00 +0200

    Add initial template for SimpleProgram

---
+++ ElementsKernel/ElementsKernel/SimpleProgram.h
@@ -0,0 +1,21 @@
+/**
+ * @file SimpleProgram.h
+ *
+ * @date Aug 27, 2014
+ * @author Hubert Degaudenzi
+ */
+
+#ifndef ELEMENTSKERNEL_SIMPLEPROGRAM_H_
+#define ELEMENTSKERNEL_SIMPLEPROGRAM_H_
+
+namespace Elements {
+
+class SimpleProgram {
+public:
+  SimpleProgram();
+  virtual ~SimpleProgram();
+};
+
+} // namespace Elements
+
+#endif // ELEMENTSKERNEL_SIMPLEPROGRAM_H_
---
+++ ElementsKernel/src/Lib/SimpleProgram.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file SimpleProgram.cpp
+ *
+ * @date Aug 27, 2014
+ * @author Hubert Degaudenzi
+ */
+
+#include <ElementsKernel/SimpleProgram.h>
+
+namespace Elements {
+
+SimpleProgram::SimpleProgram()
+{
+  // @todo Auto-generated constructor stub
+
+
+}
+
+SimpleProgram::~SimpleProgram()
+{
+  // @todo Auto-generated destructor stub
+}
+
+} // namespace Elements
