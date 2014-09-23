7820d1a60d9d19198c8a71ecf5fbc40ffbd199e3
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-09-23 11:14:11 +0000

    Reintegrate the PythonProgram branch
    This is the development by Nikos for the support of pure python
    program.
    
    
    
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@5764 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ElementsExamples/python/ElementsExamples/PythonProgramExample.py
@@ -0,0 +1,33 @@
+import argparse
+import ElementsKernel.Logging as log
+
+def defineSpecificProgramOptions():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--string-value', type=str, help='A string option')
+    parser.add_argument('--int-value', type=int, help='An integer option')
+    parser.add_argument('--f-value', type=float, help='A float option')
+    parser.add_argument('--int-list', nargs='+', type=int, help='An int list')
+    parser.add_argument('--float-list', nargs='+', type=float, help='An float list')
+    return parser
+
+
+def mainMethod(args):
+    logger = log.getLogger('PyhtonProgramExample')
+    
+    logger.info('#')
+    logger.info('#  Logging from the mainMethod() of the PythonProgramExample ')
+    logger.info('#')
+    
+    try:
+        logger.info('#')
+        logger.info('# Adding the values of the int list')
+        if not args.int_list:
+            raise Exception('--int-list parameter missing')
+        sum = 0
+        for i in args.int_list:
+            logger.debug('Adding %d', i)
+            sum += i
+        logger.info('Sum = %d', sum)
+    except Exception as e:
+        logger.error('Something went wrong...')
+        logger.exception(e)
