a09e6f67ffcf2159a8b62d380a7c0aee63eb6106
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-10-08 18:07:10 +0200

    Add Temporary python module for test example

---
+++ ElementsKernel/tests/python/TempTest.py
@@ -0,0 +1,33 @@
+from ElementsKernel.Temporary import TempDir
+import os
+
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    """ test case for the temporary directory class """
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.samplebasename = "toto"
+        self.tmpdir = TempDir(suffix="tempdir", prefix=self.samplebasename)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testName(self):
+        name1 = str(self.tmpdir)
+        name2 = self.tmpdir.getName()
+        # the next line should print twice the same thing
+        print self.tmpdir, name2, " ",
+        self.assert_(name1 == name2)
+
+    def testDestruction(self):
+        mydir = TempDir(suffix="tempdir", prefix="mydir")
+        mydirname = mydir.getName()
+        del mydir
+        # the temporary directory should have been removed
+        self.assert_(not os.path.exists(mydirname))
+if __name__ == '__main__':
+    unittest.main()
