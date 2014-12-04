b1e0ec036e5c19c0e1b3d246248a67d66b35f322
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-12-04 13:24:21 +0100

    Add the ThisModule.h header file

---
+++ ElementsKernel/ElementsKernel/ThisModule.h
@@ -0,0 +1,38 @@
+/**
+ * @file ThisModule.h
+ *
+ * @date Dec 1, 2014
+ * @author hubert
+ */
+
+#ifndef ELEMENTSKERNEL_ELEMENTSKERNEL_THISMODULE_H_
+#define ELEMENTSKERNEL_ELEMENTSKERNEL_THISMODULE_H_
+
+
+namespace Elements {
+namespace System {
+
+
+static std::unique_ptr<Dl_info> this_module;
+
+static inline const Dl_info& getThisModuleInfo() {
+
+  if ( this_module == nullptr) {
+    this_module.reset(new Dl_info);
+    ::dladdr((void*)getThisModuleInfo, this_module.get());
+  }
+
+  return *this_module;
+}
+
+
+static inline std::string getThisModuleName() {
+  return std::string(getThisModuleInfo().dli_fname);
+}
+
+} // namespace System
+} // namespace Elements
+
+
+
+#endif // ELEMENTSKERNEL_ELEMENTSKERNEL_THISMODULE_H_
