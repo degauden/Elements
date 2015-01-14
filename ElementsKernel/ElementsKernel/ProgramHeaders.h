c84ef720e98a0d2301c33ae4ea45dd063b71cb9f
Pierre Dubath <Pierre.Dubath@unige.ch>
2015-01-14 17:16:54 +0000

    A couple of additional headers inserted in ProgramHeaders with some comments
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@6727 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/ElementsKernel/ProgramHeaders.h
+++ ElementsKernel/ElementsKernel/ProgramHeaders.h
@@ -3,11 +3,17 @@
 
 // there is no version of boost with the std::__debug namespace
 #include "ElementsKernel/NoGlibDebug.h"
-#include <boost/program_options.hpp>
-#include "ElementsKernel/Logging.h"
-#include "ElementsKernel/Exception.h"
-#include "ElementsKernel/Program.h"
-#include "ElementsKernel/Main.h"
-#include "ElementsKernel/Version.h"
+
+#include <boost/program_options.hpp>        // for program options from configuration file of command line arguments
+#include <boost/filesystem.hpp>             // for boost path type
+
+#include "ElementsKernel/Exit.h"            // for ExitCode, ExitCode::OK
+#include "ElementsKernel/Logging.h"         // for Logging::LogMessageStream, etc
+#include "ElementsKernel/Exception.h"       // for Elements Exception
+
+#include "ElementsKernel/Main.h"            // for the MAIN macro
+#include "ElementsKernel/Program.h"         // for the Program interface
+
+#include "ElementsKernel/Version.h"         // ??
 
 #endif
