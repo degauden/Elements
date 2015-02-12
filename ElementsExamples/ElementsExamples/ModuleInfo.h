c60d403364b55fca60a5a064526b11def1872394
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-02-12 15:08:01 +0100

    Add module test in the ElementsExample module

---
+++ ElementsExamples/ElementsExamples/ModuleInfo.h
@@ -0,0 +1,20 @@
+/**
+ * @file ModuleInfo.h
+ *
+ * @date Feb 12, 2015
+ * @author hubert
+ */
+
+#ifndef ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_MODULEINFO_H_
+#define ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_MODULEINFO_H_
+
+#include "ElementsKernel/Export.h"
+#include "ElementsKernel/ModuleInfo.h"
+
+namespace Elements {
+
+ELEMENTS_API const System::ModuleInfo& getModuleInfo();
+
+} // namespace Elements
+
+#endif // ELEMENTSEXAMPLES_ELEMENTSEXAMPLES_MODULEINFO_H_
---
+++ ElementsExamples/src/Lib/ModuleInfo.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file ExampleModuleInfo.cpp
+ *
+ * @date Feb 12, 2015
+ * @author hubert
+ */
+
+
+#include "ElementsKernel/ThisModule.h"
+#include "ElementsExamples/ModuleInfo.h"
+
+namespace Elements {
+
+const System::ModuleInfo& getModuleInfo() {
+
+  return System::getThisModuleInfo();
+
+}
+
+} // namespace Elements
+
+
