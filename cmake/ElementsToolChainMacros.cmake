81b26c297839ef2322cd938239f8e96cfd972a4a
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-03-13 15:35:30 +0100

    #10: Fix the bootstrap of the CMake library location
    Task-Url: http://degauden.isdc.unige.ch/trac/elements/ticket/10
    
    Add function to preload the local cmake dirs

--- cmake/ElementsToolChainMacros.cmake
+++ cmake/ElementsToolChainMacros.cmake
@@ -3,3 +3,23 @@ CMAKE_MINIMUM_REQUIRED(VERSION 2.8.9)
 include(ElementsUtils)
 
 
+macro(preload_local_module_path)
+
+  # Preset the CMAKE_MODULE_PATH from the environment, if not already defined.
+  if(NOT CMAKE_MODULE_PATH)
+    # Note: this works even if the envirnoment variable is not set.
+    file(TO_CMAKE_PATH "$ENV{CMAKE_MODULE_PATH}" CMAKE_MODULE_PATH)
+  endif()
+
+  # Automatically add the modules directory provided by the project.
+  if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake)
+    if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake/modules)
+      set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/modules ${CMAKE_MODULE_PATH})
+    endif()
+    set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake ${CMAKE_MODULE_PATH})
+  endif()
+
+  # Remove duplicates
+  list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
+
+endmacro()
