131eef428a24ef4be18256ed4b2bb1f19f47b510
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-05-22 17:58:15 +0200

    Factor out the preload of the toolchain module path
    Task-Url: http://degauden.isdc.unige.ch/trac/elements/ticket/10

--- cmake/ElementsToolChain.cmake
+++ cmake/ElementsToolChain.cmake
@@ -4,18 +4,12 @@ message(STATUS "----------------------------------------------------------------
 message(STATUS "                  --- ElementsToolChain ---                            ")
 message(STATUS "-----------------------------------------------------------------------")
 
-# Bootstrap the directory containing this very file
-if(IS_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/modules)
-  set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/modules ${CMAKE_MODULE_PATH})
-endif()
+
+# Initial bootstrap to locate the Toolchain macros
 set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_MODULE_PATH})
 
-# Remove duplicates
-list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
-
 # Loading the ToolChain library macros and functions
-
-INCLUDE(ElementsToolChainMacros)
+include(ElementsToolChainMacros)
 
 init()
 find_projects(projects ${CMAKE_SOURCE_DIR}/CMakeLists.txt)
@@ -26,17 +20,15 @@ debug_print_var(CMAKE_MODULE_PATH)
 debug_print_var(CMAKE_SOURCE_DIR)
 
 
-include(tests/UtilsTests)
+# include(tests/UtilsTests)
 
 
-debug_message(STATUS "-----------------------------------------------------------------------")
-
-set(HAS_ELEMENTS_TOOLCHAIN ON)
 
 
 message(STATUS "-----------------------------------------------------------------------")
 message(STATUS "                  --- End of ElementsToolChain ---                     ")
 message(STATUS "-----------------------------------------------------------------------")
 
+set(HAS_ELEMENTS_TOOLCHAIN ON)
 
 endif()
