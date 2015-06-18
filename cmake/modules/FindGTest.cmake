83c0ab1f19e301af60034eda4291cd958a0449a0
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-06-18 14:23:07 +0200

    Add The support for GTest

---
+++ cmake/modules/FindGTest.cmake
@@ -0,0 +1,14 @@
+if(NOT GTEST_FOUND)
+
+#  find_path(GTEST_INCLUDE_DIRS gtest )
+  find_path(GTEST_INCLUDE_DIRS gtest PATHS $ENV{GTEST_INSTALL_DIR}/include)
+
+#  find_library(GTEST_LIBRARIES NAMES gtest gtest_main )
+  find_library(GTEST_LIBRARIES NAMES gtest gtest_main PATHS $ENV{GTEST_INSTALL_DIR})
+
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTest DEFAULT_MSG GTEST_INCLUDE_DIRS GTEST_LIBRARIES)
+
+  mark_as_advanced(GTEST_FOUND GTEST_INCLUDE_DIRS GTEST_LIBRARIES)
+
+endif()
