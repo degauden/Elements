cd57f2c2cf076bc48454b7fc90159c9c59804360
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-06-22 17:10:11 +0200

    Fix the GTest CMake module

--- cmake/modules/FindGTest.cmake
+++ cmake/modules/FindGTest.cmake
@@ -1,14 +1,21 @@
 if(NOT GTEST_FOUND)
 
-#  find_path(GTEST_INCLUDE_DIRS gtest )
-  find_path(GTEST_INCLUDE_DIRS gtest PATHS $ENV{GTEST_INSTALL_DIR}/include)
+find_path(GTEST_INCLUDE_DIR gtest/gtest.h
+          HINTS $ENV{GTEST_INSTALL_DIR}/include )
 
-#  find_library(GTEST_LIBRARIES NAMES gtest gtest_main )
-  find_library(GTEST_LIBRARIES NAMES gtest gtest_main PATHS $ENV{GTEST_INSTALL_DIR})
+find_library(GTEST_LIBRARY NAMES gtest gtest_main
+             HINTS $ENV{GTEST_INSTALL_DIR} )
 
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTest DEFAULT_MSG GTEST_INCLUDE_DIRS GTEST_LIBRARIES)
+set(GTEST_LIBRARIES ${GTEST_LIBRARY} )
+set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR} )
 
-  mark_as_advanced(GTEST_FOUND GTEST_INCLUDE_DIRS GTEST_LIBRARIES)
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(GTest  DEFAULT_MSG
+                                  GTEST_LIBRARY GTEST_INCLUDE_DIR)
+
+mark_as_advanced(GTEST_INCLUDE_DIR GTEST_LIBRARY )
 
 endif()
+
