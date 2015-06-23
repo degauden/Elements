aeb48d91ffd398f7d7ca56d09857b9e335d0e596
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-06-23 14:43:07 +0200

    Chain find_packages
    - GTest is automatically dragged in with GMock
    - Cfitsio is automatically dragged in by CCFits

--- cmake/modules/FindGMock.cmake
+++ cmake/modules/FindGMock.cmake
@@ -1,10 +1,8 @@
 if(NOT GMOCK_FOUND)
 
-#find_path(GMOCK_INCLUDE_DIR gmock/gmock.h)
 find_path(GMOCK_INCLUDE_DIR gmock/gmock.h
           HINTS $ENV{GMOCK_INSTALL_DIR}/include )
 
-#find_library(GMOCK_LIBRARY NAMES gmock gmock_main )
 find_library(GMOCK_LIBRARY NAMES gmock gmock_main
              HINTS $ENV{GMOCK_INSTALL_DIR} )
 
@@ -18,5 +16,13 @@ find_package_handle_standard_args(GMock  DEFAULT_MSG
                                   GMOCK_LIBRARY GMOCK_INCLUDE_DIR)
 
 mark_as_advanced(GMOCK_FOUND GMOCK_INCLUDE_DIR GMOCK_LIBRARY)
+
+find_package(GTest)
+
+set(GMOCK_LIBRARIES ${GMOCK_LIBRARIES} ${GTEST_LIBRARY} )
+set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIRS} ${GTEST_INCLUDE_DIR} )
+
+list(REMOVE_DUPLICATES GMOCK_LIBRARIES)
+list(REMOVE_DUPLICATES GMOCK_INCLUDE_DIRS)
 
 endif()
