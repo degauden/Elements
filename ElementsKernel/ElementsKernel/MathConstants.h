d1779e2f63544fed4b4e3be0f67f1bf9d1b37aba
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-06-25 09:20:53 +0000

    Add a few constants
    - sqrt(pi)
    - sqrt(2) 
    - sqrt(pi/2)
    
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@8018 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/ElementsKernel/MathConstants.h
+++ ElementsKernel/ElementsKernel/MathConstants.h
@@ -12,19 +12,25 @@ namespace Elements {
 namespace Units {
 
 /// @brief \f$\pi\f$
-static const double     pi  = 3.14159265358979323846;
+static const double     pi         = 3.14159265358979323846;
 /// @brief \f$2\pi\f$
-static const double  twopi  = 2.0*pi;
+static const double  twopi         = 2.0*pi;
 /// @brief \f$\pi/2\f$
-static const double halfpi  = pi/2.0;
+static const double halfpi         = pi/2.0;
 /// @brief \f$\pi^2\f$
-static const double     pi2 = pi*pi;
+static const double     pi2        = pi*pi;
+/// @brief \f$\sqrt{\pi}\f$
+static const double sqrt_of_pi     = 1.77245385090551602730;
+/// @brief \f$\sqrt{2}\f$
+static const double sqrt_of_two    = 1.41421356237309504880;
+/// @brief \f$\sqrt{\frac{\pi}{2}}\f$
+static const double sqrt_of_halfpi = 1.25331413731550025121;
 
 
-} // namespace Constants
-} // namespace Elements
+}  // namespace Units
+}  // namespace Elements
 
 
 
 
-#endif /* ELEMENTSKERNEL_MATHCONSTANTS_H_ */
+#endif  // ELEMENTSKERNEL_MATHCONSTANTS_H_
