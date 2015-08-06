1217cd427b65e22773a18a1184a873a1e6c3c1a9
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-08-06 11:11:14 +0200

    Fix minor sonar warnings

--- ElementsExamples/python/ElementsExamples/PythonProgramExample.py
+++ ElementsExamples/python/ElementsExamples/PythonProgramExample.py
@@ -1,23 +1,26 @@
 import argparse
 import ElementsKernel.Logging as log
 
+
 def defineSpecificProgramOptions():
     parser = argparse.ArgumentParser()
     parser.add_argument('--string-value', type=str, help='A string option')
     parser.add_argument('--int-value', type=int, help='An integer option')
     parser.add_argument('--f-value', type=float, help='A float option')
     parser.add_argument('--int-list', nargs='+', type=int, help='An int list')
-    parser.add_argument('--float-list', nargs='+', type=float, help='An float list')
+    parser.add_argument(
+        '--float-list', nargs='+', type=float, help='An float list')
     return parser
 
 
 def mainMethod(args):
     logger = log.getLogger('PythonProgramExample')
-    
+
     logger.info('#')
-    logger.info('#  Logging from the mainMethod() of the PythonProgramExample ')
+    logger.info(
+        '#  Logging from the mainMethod() of the PythonProgramExample ')
     logger.info('#')
-    
+
     try:
         logger.info('#')
         logger.info('# Adding the values of the int list')
