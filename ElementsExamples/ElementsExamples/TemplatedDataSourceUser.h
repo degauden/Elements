4c3d9db57a8a6e84d4b4c74c0ba8b16ce2d27344
Pierre Dubath <Pierre.Dubath@unige.ch>
2015-09-28 08:41:56 +0000

    reintegration of the ElementsExamples branch
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@9247 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ElementsExamples/ElementsExamples/TemplatedDataSourceUser.h
@@ -0,0 +1,51 @@
+/*
+ * TemplatedDataSourceUser.h
+ *
+ *  Created on: Sep 23, 2015
+ *      Author: fdubath
+ */
+
+#ifndef EXAMPLEMODULE_EXAMPLEMODULE_TEMPLATEDDATASOURCEUSER_H_
+#define EXAMPLEMODULE_EXAMPLEMODULE_TEMPLATEDDATASOURCEUSER_H_
+
+
+
+namespace Elements{
+namespace ElementsExamples {
+
+
+
+/**
+ * @class Euclid::PhzModeling::TemplatedDataSourceUser
+ * @brief This class has been created to demonstrate unit testing. It manipulate
+ * an object representing a DataSource using template to achieve polymorphism.
+ */
+class TemplatedDataSourceUser{
+public:
+
+  /**
+     * @brief Compute the sum of the values of the records stored into the provided
+     * DataSource
+     *
+     * @template T
+     * A type representing a DataSource. The type must declare the methods
+     * + size_t countRecords() const
+     * + double getRecordValue(size_t index) const
+     *
+     * @param data_source
+     * A reference to a DataSource object.
+     *
+     * @return The sum of the values of the records into the DataSource object
+     */
+  template <typename T>
+  double sumRecords(const T& data_source);
+};
+
+} // namespace ElementsExamples
+} // namespace Elements
+
+
+
+#include "ElementsExamples/_impl/TemplatedDataSourceUser.icpp"
+
+#endif /* EXAMPLEMODULE_EXAMPLEMODULE_TEMPLATEDDATASOURCEUSER_H_ */
