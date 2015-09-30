4cff5c9ce042db799261ffdc1dc25a8f89b6ecdf
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2015-09-30 12:38:42 +0000

    Added more comments
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@9336 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/auxdir/templates/PythonProgram_template.py
+++ ElementsKernel/auxdir/templates/PythonProgram_template.py
@@ -8,6 +8,15 @@ import argparse
 import ElementsKernel.Logging as log
 
 def defineSpecificProgramOptions():
+    """
+    @brief Allows to define the (command line and configuration file) options 
+    specific to this program
+    
+    @details
+        See the Elements documentation for more details.
+    @return
+        An  ArgumentParser. 
+    """                
     
     parser = argparse.ArgumentParser()
     
@@ -20,11 +29,24 @@ def defineSpecificProgramOptions():
 
 
 def mainMethod(args):
-    logger = log.getLogger('%(PROGRAMNAME)s')
+    """
+    @brief The "main" method.
+    @details
+        This method is the entry point to the program. In this sense, it is 
+        similar to a main (and it is why it is called mainMethod()).
+    """
 
+    logger = log.getLogger('%(PROGRAMNAME)s')
+    
     logger.info('#')
-    logger.info('#  Logging from the mainMethod() of the %(PROGRAMNAME)s')
+    logger.info('# Entering %(PROGRAMNAME)s mainMethod()')
     logger.info('#')
+    
+    # !! Getting the option from the example option in defineSpecificProgramOption 
+    # !! e.g string_option = args.string_value
+    
     #
     # !!! Write you main code here !!! 
     #
+    
+    logger.info('Exiting %(PROGRAMNAME)s mainMethod()')
