4f372bf697660352043d8a61d91513c99cc649a7
Florian Dubath <Florian.Dubath@unige.ch>
2015-10-02 08:38:15 +0000

    update unit test
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@9373 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsExamples/tests/src/Boost/UnitTestExample_test.cpp
+++ ElementsExamples/tests/src/Boost/UnitTestExample_test.cpp
@@ -17,6 +17,45 @@ using namespace Elements::ElementsExamples;
 // Starts a test suite and name it.
 BOOST_AUTO_TEST_SUITE (UnitTestExample_test_suite)
 
+
+BOOST_AUTO_TEST_CASE(median_test) {
+  UnitTestExample tested_object{};
+  std::vector<std::vector<int>> input_vec {{1, 2, 3, 10, 101, 102},
+                                           {1, 2, 3, 11, 101, 102, 103},
+                                           {1, 2, 3, 10, 101, 102, 103, 500},
+                                           {1, 2, 3, 11, 101, 102, 103, 500, 501}
+                                          };
+  std::vector<double> expected_result {6.5, 11, 55.5, 101};
+
+  for(int test_id=0; test_id<4;++test_id){
+    BOOST_CHECK_EQUAL(expected_result[test_id], tested_object.average(input_vec[test_id]));
+  }
+}
+
+
+BOOST_AUTO_TEST_CASE(order_test) {
+  UnitTestExample tested_object{};
+  std::vector<int> input_vec_orderd {1, 2, 3, 11, 101, 102, 103};
+  std::vector<int> input_vec{103, 101, 2 ,1 , 3, 11, 102};
+  double expected_result {11};
+
+  BOOST_CHECK_EQUAL(expected_result, tested_object.average(input_vec_orderd));
+  BOOST_CHECK_EQUAL(expected_result, tested_object.average(input_vec));
+}
+
+
+BOOST_AUTO_TEST_CASE(mean_test) {
+  UnitTestExample tested_object{};
+  std::vector<std::vector<int>> input_vec {{1},{1,2},{1,2,4},{1,2,4,8},{1,2,4,8,16}};
+  std::vector<double> expected_result {1,1.5,2.33333333333333333,3.75,6.2};
+  double tolerence {1e-10};
+
+  for(int test_id=0; test_id<5;++test_id){
+    BOOST_CHECK_CLOSE(expected_result[test_id], tested_object.average(input_vec[test_id]), tolerence);
+  }
+}
+
+
 BOOST_AUTO_TEST_CASE(exception_test) {
   UnitTestExample tested_object{};
   std::vector<int> input_vec {};
@@ -24,22 +63,6 @@ BOOST_AUTO_TEST_CASE(exception_test) {
   BOOST_CHECK_THROW(tested_object.average(input_vec),Elements::Exception);
 }
 
-BOOST_AUTO_TEST_CASE(median_test) {
-  UnitTestExample tested_object{};
-  std::vector<int> input_vec {1, 2, 3, 11, 101, 102, 103};
-  double expected_result {11};
-
-  BOOST_CHECK_EQUAL(expected_result, tested_object.average(input_vec));
-}
-
-BOOST_AUTO_TEST_CASE(mean_test) {
-  UnitTestExample tested_object{};
-  std::vector<int> input_vec {1, 2, 14};
-  double expected_result {5.666666666666};
-  double tolerence {1e-10};
-
-  BOOST_CHECK_CLOSE(expected_result, tested_object.average(input_vec), tolerence);
-}
 
 // Ends the test suite
 BOOST_AUTO_TEST_SUITE_END ()
