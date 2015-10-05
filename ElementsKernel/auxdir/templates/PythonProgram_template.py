e5347932de90bd12f6703cacbb70d04ab9a37f76
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2015-10-05 15:40:55 +0000

    Fixed problem arose by pylint
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@9438 463500fe-d6be-4184-9b20-47717e75ecac

--- ElementsKernel/auxdir/templates/PythonProgram_template.py
+++ ElementsKernel/auxdir/templates/PythonProgram_template.py
@@ -1,30 +1,30 @@
-##
-# @file: %(FILE)s
-# @author: %(AUTHOR)s
-# @date: %(DATE)s
-##
+"""
+@file: %(FILE)s
+@author: %(AUTHOR)s
+@date: %(DATE)s
+"""
 
 import argparse
 import ElementsKernel.Logging as log
 
 def defineSpecificProgramOptions():
     """
-    @brief Allows to define the (command line and configuration file) options 
+    @brief Allows to define the (command line and configuration file) options
     specific to this program
     
     @details
         See the Elements documentation for more details.
     @return
-        An  ArgumentParser. 
-    """                
-    
+        An  ArgumentParser.
+    """
+
     parser = argparse.ArgumentParser()
-    
+
     #
     # !!! Write your program options here !!!
     # e.g. parser.add_argument('--string-value', type=str, help='A string option')
     #
-    
+
     return parser
 
 
@@ -37,19 +37,18 @@ def mainMethod(args):
     """
 
     logger = log.getLogger('%(PROGRAMNAME)s')
-    
+
     logger.info('#')
     logger.info('# Entering %(PROGRAMNAME)s mainMethod()')
     logger.info('#')
-    
+
     # !! Getting the option from the example option in defineSpecificProgramOption 
     # !! e.g string_option = args.string_value
-    
+
     #
     # !!! Write you main code here !!! 
     #
-    
+
     logger.info('#')
     logger.info('# Exiting %(PROGRAMNAME)s mainMethod()')
     logger.info('#')
-   
