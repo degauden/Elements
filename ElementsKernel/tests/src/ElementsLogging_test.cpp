82331bf4a732645c68ae591600c4a41346997f4c
Hubert Degaudenzi <Hubert.Degaudenzi@gmail.com>
2015-11-19 15:05:16 +0100

    Fix a few C++ warnings from eclipse

--- ElementsKernel/tests/src/ElementsLogging_test.cpp
+++ ElementsKernel/tests/src/ElementsLogging_test.cpp
@@ -44,7 +44,7 @@ using std::tuple;
 // sent to it. It recovers the given stream in its previous state during destruction.
 class LogMessageTracker {
  public:
-  LogMessageTracker(std::ostream& stream) :
+  explicit LogMessageTracker(std::ostream& stream) :
       m_messages { }, m_stream(stream), m_old { stream.rdbuf(m_messages.rdbuf()) } {
   }
   ~LogMessageTracker() {
