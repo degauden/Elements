21a2fbae8f8be9937847a01a7e2e14fadbaef60b
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-12-23 11:46:11 +0100

    Fix the casing for the FIND_PACKAGE_HANDLE_STANDARD_ARGS macro
    The first argument of that macro has to match exactly the name of the
    file (FindXXX.cmake). With the exact casing.

--- cmake/modules/FindTar.cmake
+++ cmake/modules/FindTar.cmake
@@ -6,7 +6,7 @@ if (NOT TAR_FOUND)
 # handle the QUIETLY and REQUIRED arguments and set TAR_FOUND to TRUE if
 # all listed variables are TRUE
     INCLUDE(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(TAR DEFAULT_MSG TAR_EXECUTABLE)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Tar DEFAULT_MSG TAR_EXECUTABLE)
 
     mark_as_advanced(TAR_FOUND TAR_EXECUTABLE)
 
