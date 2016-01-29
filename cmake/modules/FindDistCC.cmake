cfded67f6355b5c8cdf9a9d0b29bc19e86acdead
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-01-29 11:50:51 +0100

    Add the FindDistCC CMake module

---
+++ cmake/modules/FindDistCC.cmake
@@ -0,0 +1,15 @@
+if (NOT DISTCC_FOUND)
+
+	find_program(DISTCC_EXECUTABLE distcc)
+	set(DISTCC_EXECUTABLE ${DISTCC_EXECUTABLE} CACHE STRING "")
+
+# handle the QUIETLY and REQUIRED arguments and set DISTCC_FOUND to TRUE if
+# all listed variables are TRUE
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(DistCC DEFAULT_MSG DISTCC_EXECUTABLE)
+
+    mark_as_advanced(DISTCC_FOUND DISTCC_EXECUTABLE)
+
+
+endif (NOT DISTCC_FOUND)
+
