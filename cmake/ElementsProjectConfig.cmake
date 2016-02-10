91ab67b007082d409fa3484d5bec3aeab4cc6797
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-02-10 13:42:05 +0000

    Factor out the CMake documentation feature into a separate file
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@11185 463500fe-d6be-4184-9b20-47717e75ecac

--- cmake/ElementsProjectConfig.cmake
+++ cmake/ElementsProjectConfig.cmake
@@ -886,33 +886,7 @@ macro(elements_project project version)
 
   endif()
 
-  # Add Doxygen generation
-  find_package(Doxygen QUIET)
-  if(DOXYGEN_FOUND)
-    find_file(doxygen_file_template
-              NAMES Doxyfile.in
-              PATHS ${CMAKE_MODULE_PATH}
-              PATH_SUFFIXES doc
-              NO_DEFAULT_PATH)
-
-
-    if(doxygen_file_template)
-      configure_file(
-        "${doxygen_file_template}"
-        "${PROJECT_BINARY_DIR}/doc/Doxyfile"
-        @ONLY
-      )
-      message(STATUS "Generated Doxygen configuration file: ${PROJECT_BINARY_DIR}/doc/Doxyfile")
-      message(STATUS "From the Doxygen.in template file: ${doxygen_file_template}")
-
-    endif()
-
-    add_custom_target(doc
-                      ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/doc/Doxyfile
-                      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc
-                      COMMENT "Generating API documentation with Doxygen" VERBATIM
-    )
-  endif()
+  include(ElementsDocumentation)
 
 endmacro()
 
