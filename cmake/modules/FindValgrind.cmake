d6e8524320a635ebedd6f2b34f40b27934a4b451
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-02-10 13:42:01 +0000

    Add FindValgrind CMake module
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@11183 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ cmake/modules/FindValgrind.cmake
@@ -0,0 +1,15 @@
+if (NOT VALGRIND_FOUND)
+
+	find_program(VALGRIND_EXECUTABLE valgrind)
+	set(VALGRIND_EXECUTABLE ${VALGRIND_EXECUTABLE} CACHE STRING "")
+
+# handle the QUIETLY and REQUIRED arguments and set VALGRIND_FOUND to TRUE if
+# all listed variables are TRUE
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Valgrind DEFAULT_MSG VALGRIND_EXECUTABLE)
+
+    mark_as_advanced(VALGRIND_FOUND VALGRIND_EXECUTABLE)
+
+
+endif (NOT VALGRIND_FOUND)
+
