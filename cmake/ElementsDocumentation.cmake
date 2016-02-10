be5ad7435505b93b499c870d701e179bb94393d1
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-02-10 13:42:09 +0000

    Add the generation of the sphinx configuration file
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@11187 463500fe-d6be-4184-9b20-47717e75ecac

--- cmake/ElementsDocumentation.cmake
+++ cmake/ElementsDocumentation.cmake
@@ -1,6 +1,7 @@
   # Add Doxygen generation
   find_package(Doxygen)
   if(DOXYGEN_FOUND)
+
     find_file(doxygen_file_template
               NAMES Doxyfile.in
               PATHS ${CMAKE_MODULE_PATH}
@@ -24,7 +25,31 @@
                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/doxygen
                       COMMENT "Generating API documentation with Doxygen" VERBATIM
     )
+
   endif()
   
+  
+  
   find_package(Sphinx)
+  if(SPHINX_FOUND)
+  
+    find_file(sphinx_conf_template
+              NAMES Sphinx_conf.py.in
+              PATHS ${CMAKE_MODULE_PATH}
+              PATH_SUFFIXES doc
+              NO_DEFAULT_PATH)
+
+
+    if(sphinx_conf_template)
+      configure_file(
+        "${sphinx_conf_template}"
+        "${PROJECT_BINARY_DIR}/doc/sphinx/conf.py"
+        @ONLY
+      )
+      message(STATUS "Generated Sphinx configuration file: ${PROJECT_BINARY_DIR}/doc/sphinx/conf.py")
+      message(STATUS "From the Doxygen.in template file: ${sphinx_conf_template}")
+
+    endif()
+  
+  endif()
   
