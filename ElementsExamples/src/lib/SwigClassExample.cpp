c86ffd3fdeb6868dd988b8c4de8f98baff3b3bed
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-02-17 13:54:51 +0000

    Add simple Python/C++ Swig generation
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Elements/trunk@11347 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ElementsExamples/src/lib/SwigClassExample.cpp
@@ -0,0 +1,20 @@
+#include "ElementsExamples/SwigClassExample.h"
+
+namespace Elements {
+namespace Examples {
+
+
+double SwigClassExample::getRa () const {
+  return m_ra;
+}
+
+
+double SwigClassExample::getDec () const {
+  return m_dec;
+}
+
+
+
+} // namespace Examples
+} // namespace Elements
+
