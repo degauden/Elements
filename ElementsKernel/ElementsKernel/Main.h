c4b85620ed4a9679a99408806d24dadc3147a860
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-04-05 17:08:34 +0200

    Created the MAIN_FOR_NOEXCEPT macro that catches all the exceptions

--- ElementsKernel/ElementsKernel/Main.h
+++ ElementsKernel/ElementsKernel/Main.h
@@ -24,11 +24,37 @@
  * must end with the following line:
  * @code
  *    MAIN_FOR(ELEMENTS_PROGRAM_NAME)
- * @endcode
+ * @endcode.
  * @param ELEMENTS_PROGRAM_NAME name of the main program class, derived from
  * the class Elements::Program class.
  */
 #define MAIN_FOR(ELEMENTS_PROGRAM_NAME)         \
+  ELEMENTS_API int main(int argc, char* argv[])              \
+  {                                             \
+    Elements::ProgramManager man {std::unique_ptr<Elements::Program>{new ELEMENTS_PROGRAM_NAME{}}, \
+                                  THIS_PROJECT_VERSION_STRING, THIS_PROJECT_NAME_STRING}; \
+    Elements::ExitCode exit_code =  man.run(argc, argv);   \
+    return static_cast<Elements::ExitCodeType>(exit_code);    \
+  }
+
+
+/** @def MAIN_FOR_NOEXCEPT(ELEMENTS_PROGRAM_NAME)
+ * Macro which must be used to create a main in classes
+ * that derived from Elements::Program, i.e., these derived classes
+ * must end with the following line:
+ * @code
+ *    MAIN_FOR_NOEXCEPT(ELEMENTS_PROGRAM_NAME)
+ * @endcode
+ * The macro is equiped with a set_terminate placed just before
+ * the main function definition:
+ * @code
+ * ELEMENTS_UNUSED const auto installed{std::set_terminate(&Elements::ProgramManager::onTerminate)};
+ * @endcode
+ * That handler will do a smooth catching of any exception raised in the run part of the program.
+ * @param ELEMENTS_PROGRAM_NAME name of the main program class, derived from
+ * the class Elements::Program class.
+ */
+#define MAIN_FOR_NOEXCEPT(ELEMENTS_PROGRAM_NAME)         \
   ELEMENTS_UNUSED const auto installed{std::set_terminate(&Elements::ProgramManager::onTerminate)}; \
   ELEMENTS_API int main(int argc, char* argv[])              \
   {                                             \
@@ -37,6 +63,8 @@
     Elements::ExitCode exit_code =  man.run(argc, argv);   \
     return static_cast<Elements::ExitCodeType>(exit_code);    \
   }
+
+
 
 #endif // ELEMENTSKERNEL_MAIN_H_
 
