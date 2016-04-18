9509de9bb9af6c4f1c19b6af7582e36cf7b5ab03
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-04-18 16:36:34 +0200

    Add a few copyright notices

--- ElementsKernel/ElementsKernel/Export.h
+++ ElementsKernel/ElementsKernel/Export.h
@@ -16,6 +16,19 @@
  *  @endcode
  *  This macro will only have an effect if the global symbol hiding is active in the framework.
  *  This means that the ELEMENTS_HIDE_SYMBOLS macro has to be defined.
+ *
+ * @copyright 2012-2020 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifndef ELEMENTS_EXPORT_H_
