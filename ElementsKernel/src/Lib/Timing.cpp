18f4dc5be47b4d06962fbc35862913ede13d6783
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-04-21 16:21:10 +0200

    Fix a few Sonar warnings

--- ElementsKernel/src/Lib/Timing.cpp
+++ ElementsKernel/src/Lib/Timing.cpp
@@ -1,16 +1,19 @@
-// $Id: Timing.cpp,v 1.3 2002/11/12 18:34:30 mato Exp $
-//====================================================================
-//	Timing.cpp
-//--------------------------------------------------------------------
-//
-//	Package    : System (The LHCb System service)
-//
-//  Description: Implementation of Systems internals
-//
-//	Author     : M.Frank
-//  Created    : 13/1/99
-//	Changes    :
-//====================================================================
+/**
+ *
+ * @copyright 2012-2020 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
 
 #include "ElementsKernel/Timing.h"
 #include "ProcessDescriptor.h"
