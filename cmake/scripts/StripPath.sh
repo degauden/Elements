5aa193b0806239d62bdaf676170f95923d9c4df8
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2016-07-14 18:02:54 +0200

    Fix wrong name of the configuration file path.
    ELEMENTS_CONF_FILES -> ELEMENTS_CONF_PATH

--- cmake/scripts/StripPath.sh
+++ cmake/scripts/StripPath.sh
@@ -1,7 +1,7 @@
 if [[ "x$ELEMENTS_NO_STRIP_PATH" == "x" ]] ; then
  scriptdir=$ELEMENTSPOLICYROOT/scripts
 
-  tmpfile=`python $scriptdir/PathStripper.py --shell=sh --mktemp -e PATH -e LD_LIBRARY_PATH -e PYTHONPATH -e ELEMENTS_AUX_PATH -e ELEMENTS_CONF_FILES -e HPATH `
+tmpfile=`python $scriptdir/PathStripper.py --shell=sh --mktemp -e PATH -e LD_LIBRARY_PATH -e PYTHONPATH -e ELEMENTS_AUX_PATH -e ELEMENTS_CONF_PATH -e HPATH `
   . $tmpfile
   rm -f $tmpfile
   unset tmpfile
