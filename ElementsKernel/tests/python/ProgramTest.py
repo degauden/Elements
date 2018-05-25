bb78126e1aff3aeaee55cecef858d7f892b9159d
Mher Kazandjian <mherkazandjian@gmail.com>
2018-05-25 19:45:26 +0200

    implement the body of the tests for checking argument precedence

--- ElementsKernel/tests/python/ProgramTest.py
+++ ElementsKernel/tests/python/ProgramTest.py
@@ -20,7 +20,9 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 '''
-import subprocess
+import os
+from subprocess import Popen, PIPE
+import shlex
 import unittest
 import pytest
 
@@ -37,37 +39,74 @@ class TestProgramTest(object):
 
 
     @pytest.mark.parametrize(
-        "default, default_conf, cmd_line_conf, cmd_line_arg",
+        "default, default_conf, cmd_line_conf, cmd_line_arg, expected",
         [
-            ("default", None, None, None),
-            ("default", "default_conf", None, None),
-            ("default", "default_conf", "cmd_line_conf", None),
-            ("default", "default_conf", "cmd_line_conf", "cmd_line_param"),
-            ("default", "default_conf", None, "cmd_line_param"),
-            ("default", None, "cmd_line_conf", None),
-            ("default", None, "cmd_line_conf", "cmd_line_param"),
-            ("default", None, None, "cmd_line_param"),
-            (None, "default_conf", None, None),
-            (None, "default_conf", "cmd_line_conf", None),
-            (None, "default_conf", "cmd_line_conf", "cmd_line_param"),
-            (None, "default_conf", None, "cmd_line_param"),
-            (None, None, "cmd_line_conf", None),
-            (None, None, "cmd_line_conf", "cmd_line_param"),
-            (None, None, None, "cmd_line_param"),
+            ("default", None,           None,            None,             111),
+            ("default", "default_conf", None,            None,             333),
+            ("default", "default_conf", "cmd_line_conf", None,             555),
+            ("default", "default_conf", "cmd_line_conf", "cmd_line_param", 666),
+            ("default", "default_conf", None,            "cmd_line_param", 666),
+            ("default", None,           "cmd_line_conf", None,             555),
+            ("default", None,           "cmd_line_conf", "cmd_line_param", 666),
+            ("default", None,           None,            "cmd_line_param", 666),
+            (None,      "default_conf", None,            None,             333),
+            (None,      "default_conf", "cmd_line_conf", None,             555),
+            (None,      "default_conf", "cmd_line_conf", "cmd_line_param", 666),
+            (None,      "default_conf", None,            "cmd_line_param", 666),
+            (None,      None,           "cmd_line_conf", None,             555),
+            (None,      None,           "cmd_line_conf", "cmd_line_param", 666),
+            (None,      None,           None,            "cmd_line_param", 666),
         ]
     )
     def test_that_parameter_precedence_is_respected(self,
                                                     default,
                                                     default_conf,
                                                     cmd_line_conf,
-                                                    cmd_line_arg):
-        prcedence = {
-            "default": 0,
-            "default_conf": 1,
-            "cmd_line_conf": 2,
-            "cmd_line_param": 3
-        }
-        process = subprocess.Popen("ERun ")
+                                                    cmd_line_arg,
+                                                    expected):
+
+        test_program_script = 'PythonProgramExample'
+
+        cmd = test_program_script
+
+        if default is not None and default_conf is None:
+            arg_to_test = "int-option-with-default-no-default-in-conf"
+        elif default is not None and default_conf is not None:
+            arg_to_test = "int-option-with-default-and-default-in-conf"
+        elif default is None and default_conf is not None:
+            arg_to_test = "int-option"
+        elif default is None and default_conf is None:
+            arg_to_test = "int-option-no-default-not-defined-in-conf"
+        else:
+            raise ValueError('unsupported test case')
+
+        if cmd_line_conf is not None:
+            cmd_line_conf_fpath = os.path.join(
+                os.environ['ELEMENTSKERNELROOT'], 'tests', 'data', 'conf',
+                test_program_script + '.conf'
+            )
+            cmd = '{} --config-file={}'.format(cmd, cmd_line_conf_fpath)
+
+        if cmd_line_arg is not None:
+            cmd = '{} --{}=666'.format(cmd, arg_to_test)
+
+        # DEBUG
+        print('\n', cmd)
+
+        process = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+        _, stderr = process.communicate()
+        output_arg_line = list(
+            filter(
+                lambda line: ' {} = '.format(arg_to_test) in line,
+                stderr.decode().splitlines()
+            )
+        )
+
+        assert len(output_arg_line) == 1, 'the argument {} should appear exactly once'.format(arg_to_test)
+
+        output_arg_value = int(output_arg_line.pop().split('=')[-1])
+
+        assert output_arg_value == expected
 
 
 if __name__ == "__main__":
