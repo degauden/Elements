67c1c45b7fe770223f0352fb8067e507c5028efc
Mher Kazandjian <mherkazandjian@gmail.com>
2018-05-25 17:09:08 +0200

    implement test skeleton for argument precedence

--- ElementsKernel/tests/python/ProgramTest.py
+++ ElementsKernel/tests/python/ProgramTest.py
@@ -20,17 +20,54 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 '''
+import subprocess
 import unittest
+import pytest
 
 from ElementsKernel.Program import Program
 
-class ProgramTestTest(unittest.TestCase):
+
+class TestProgramTest(object):
+
+    def test_that_instance_can_be_created_without_raising_exception(self):
+        _ = Program("ElementsExamples.PythonProgramExample")
+
+    def test_that_instance_can_be_created_without_raising_exception_for_a_blank_original_path(self):
+        _ = Program("ElementsExamples.PythonProgramExample", original_path="")
 
 
-    def testConstructor(self):
-        # The following lines should not raise any exception
-        p = Program("ElementsExamples.PythonProgramExample")
-        p2 = Program("ElementsExamples.PythonProgramExample", original_path="")
+    @pytest.mark.parametrize(
+        "default, default_conf, cmd_line_conf, cmd_line_arg",
+        [
+            ("default", None, None, None),
+            ("default", "default_conf", None, None),
+            ("default", "default_conf", "cmd_line_conf", None),
+            ("default", "default_conf", "cmd_line_conf", "cmd_line_param"),
+            ("default", "default_conf", None, "cmd_line_param"),
+            ("default", None, "cmd_line_conf", None),
+            ("default", None, "cmd_line_conf", "cmd_line_param"),
+            ("default", None, None, "cmd_line_param"),
+            (None, "default_conf", None, None),
+            (None, "default_conf", "cmd_line_conf", None),
+            (None, "default_conf", "cmd_line_conf", "cmd_line_param"),
+            (None, "default_conf", None, "cmd_line_param"),
+            (None, None, "cmd_line_conf", None),
+            (None, None, "cmd_line_conf", "cmd_line_param"),
+            (None, None, None, "cmd_line_param"),
+        ]
+    )
+    def test_that_parameter_precedence_is_respected(self,
+                                                    default,
+                                                    default_conf,
+                                                    cmd_line_conf,
+                                                    cmd_line_arg):
+        prcedence = {
+            "default": 0,
+            "default_conf": 1,
+            "cmd_line_conf": 2,
+            "cmd_line_param": 3
+        }
+        process = subprocess.Popen("ERun ")
 
 
 if __name__ == "__main__":
