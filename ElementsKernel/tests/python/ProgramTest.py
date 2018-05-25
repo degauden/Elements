c53296e30a2be67039e9104231114c7aee61a81e
Mher Kazandjian <mherkazandjian@gmail.com>
2018-05-25 20:50:00 +0200

    cover more use cases of argument precedence
    - pass the argument name to be tests in the  parametrized list
    - add dedicated argument for testing the case where the argument default are not defined anywhere

--- ElementsKernel/tests/python/ProgramTest.py
+++ ElementsKernel/tests/python/ProgramTest.py
@@ -39,23 +39,23 @@ class TestProgramTest(object):
 
 
     @pytest.mark.parametrize(
-        "default, default_conf, cmd_line_conf, cmd_line_arg, expected",
+        "default, default_conf, cmd_line_conf, cmd_line_arg, arg_name, expected",
         [
-            ("default", None,           None,            None,             111),
-            ("default", "default_conf", None,            None,             333),
-            ("default", "default_conf", "cmd_line_conf", None,             555),
-            ("default", "default_conf", "cmd_line_conf", "cmd_line_param", 666),
-            ("default", "default_conf", None,            "cmd_line_param", 666),
-            ("default", None,           "cmd_line_conf", None,             555),
-            ("default", None,           "cmd_line_conf", "cmd_line_param", 666),
-            ("default", None,           None,            "cmd_line_param", 666),
-            (None,      "default_conf", None,            None,             333),
-            (None,      "default_conf", "cmd_line_conf", None,             555),
-            (None,      "default_conf", "cmd_line_conf", "cmd_line_param", 666),
-            (None,      "default_conf", None,            "cmd_line_param", 666),
-            (None,      None,           "cmd_line_conf", None,             555),
-            (None,      None,           "cmd_line_conf", "cmd_line_param", 666),
-            (None,      None,           None,            "cmd_line_param", 666),
+            ("default", None,           None,            None,             "int-option-with-default-no-default-in-conf",  444),
+            ("default", "default_conf", None,            None,             "int-option-with-default-and-default-in-conf", 333),
+            ("default", "default_conf", "cmd_line_conf", None,             "int-option-with-default-and-default-in-conf", 777),
+            ("default", "default_conf", "cmd_line_conf", "cmd_line_param", "int-option-with-default-and-default-in-conf", 666),
+            ("default", "default_conf", None,            "cmd_line_param", "int-option-with-default-and-default-in-conf", 666),
+            ("default", None,           "cmd_line_conf", None,             "int-option-with-default-no-default-in-conf", 888),
+            ("default", None,           "cmd_line_conf", "cmd_line_param", "int-option-with-default-no-default-in-conf", 666),
+            ("default", None,           None,            "cmd_line_param", "int-option-with-default-no-default-in-conf", 666),
+            (None,      "default_conf", None,            None,             "int-option", 3),
+            (None,      "default_conf", "cmd_line_conf", None,             "int-option", 4),
+            (None,      "default_conf", "cmd_line_conf", "cmd_line_param", "int-option", 666),
+            (None,      "default_conf", None,            "cmd_line_param", "int-option", 666),
+            (None,      None,           "cmd_line_conf", None,             "int-option-no-default-not-defined-in-conf", 555),
+            (None,      None,           "cmd_line_conf", "cmd_line_param", "int-option-no-default-not-defined-in-conf", 666),
+            (None,      None,           None,            "cmd_line_param", "int-option-with-no-defaults-anywhere", 666),
         ]
     )
     def test_that_parameter_precedence_is_respected(self,
@@ -63,22 +63,12 @@ class TestProgramTest(object):
                                                     default_conf,
                                                     cmd_line_conf,
                                                     cmd_line_arg,
+                                                    arg_name,
                                                     expected):
 
         test_program_script = 'PythonProgramExample'
 
         cmd = test_program_script
-
-        if default is not None and default_conf is None:
-            arg_to_test = "int-option-with-default-no-default-in-conf"
-        elif default is not None and default_conf is not None:
-            arg_to_test = "int-option-with-default-and-default-in-conf"
-        elif default is None and default_conf is not None:
-            arg_to_test = "int-option"
-        elif default is None and default_conf is None:
-            arg_to_test = "int-option-no-default-not-defined-in-conf"
-        else:
-            raise ValueError('unsupported test case')
 
         if cmd_line_conf is not None:
             cmd_line_conf_fpath = os.path.join(
@@ -88,25 +78,32 @@ class TestProgramTest(object):
             cmd = '{} --config-file={}'.format(cmd, cmd_line_conf_fpath)
 
         if cmd_line_arg is not None:
-            cmd = '{} --{}=666'.format(cmd, arg_to_test)
-
-        # DEBUG
-        print('\n', cmd)
+            cmd = '{} --{}=666'.format(cmd, arg_name)
 
         process = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
         _, stderr = process.communicate()
         output_arg_line = list(
             filter(
-                lambda line: ' {} = '.format(arg_to_test) in line,
+                lambda line: ' {} = '.format(arg_name) in line,
                 stderr.decode().splitlines()
             )
         )
 
-        assert len(output_arg_line) == 1, 'the argument {} should appear exactly once'.format(arg_to_test)
+        assert len(output_arg_line) == 1, 'the argument {} should appear exactly once'.format(arg_name)
 
         output_arg_value = int(output_arg_line.pop().split('=')[-1])
 
-        assert output_arg_value == expected
+        msg = (
+            'The command\n\t{}\nfailed while checking for the value of argument\n\t{}\n'
+            'The obtained value is "{}" while the expected value is "{}"'
+        ).format(
+            cmd,
+            arg_name,
+            output_arg_value,
+            expected
+        )
+        # print(msg)
+        assert output_arg_value == expected, msg
 
 
 if __name__ == "__main__":
