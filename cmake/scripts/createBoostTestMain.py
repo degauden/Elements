49cc54045a5e4bb05735d6b0ac9ecb3e8aa8f928
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2018-12-04 16:55:35 +0100

    Add the copyright in the generated file

--- cmake/scripts/createBoostTestMain.py
+++ cmake/scripts/createBoostTestMain.py
@@ -26,7 +26,28 @@ def main():
         os.makedirs(outdir)
 
     # Prepare data to be written
-    outputdata = """ /* Automatically generated file: do not modify! */
+    outputdata = """/* Automatically generated file: do not modify! */
+/**
+ * @file BoostTestMain.cpp
+ *
+ * @date Feb 8, 2017
+ * @author Hubert Degaudenzi
+ *
+ * @copyright 2012-2020 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *
+ */
 #pragma GCC visibility push(default)
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE %(pack)s Test Suite
