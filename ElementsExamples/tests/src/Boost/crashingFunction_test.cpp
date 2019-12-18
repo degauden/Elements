f8d09f00838ab5cefa394db34720d90ba6ba8c3f
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2019-12-18 10:10:18 +0100

    Add standalone test for the crashingFunction

---
+++ ElementsExamples/tests/src/Boost/crashingFunction_test.cpp
@@ -0,0 +1,42 @@
+/*
+ * Rectangle_test.cpp
+ *
+ *  Created on: Dec 17, 2019
+ *      Author: Hubert Degaudenzi
+ *
+ * @copyright 2012-2020 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+#include "ElementsExamples/crashingFunction.h"  // Access the objects you want to test
+
+
+#include <iostream>
+#include <stdexcept>                           // for the logic_error
+#include <boost/test/unit_test.hpp>
+
+using Elements::Examples::crashingFunction;
+
+// Starts a test suite and name it.
+BOOST_AUTO_TEST_SUITE(crashingFunction_test_suite)
+
+BOOST_AUTO_TEST_CASE(Call_test) {
+
+  BOOST_CHECK_THROW(crashingFunction(), std::logic_error);
+
+}
+
+
+// Ends the test suite
+BOOST_AUTO_TEST_SUITE_END()
