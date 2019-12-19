c97b29fcde5fc1725da022d13d15166cd84a982f
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2019-12-19 12:41:47 +0100

    Add test on the functions returning locations, existing or not.

--- ElementsKernel/tests/src/Configuration_test.cpp
+++ ElementsKernel/tests/src/Configuration_test.cpp
@@ -23,14 +23,16 @@
 
 #include <string>                         // for std::string
 #include <vector>                         // for std::vector
-#include <algorithm>                      // for for_each, transform
+#include <algorithm>                      // for for_each, transform, copy_if
 
 #include <boost/test/unit_test.hpp>       // for boost unit test macros
 #include <boost/filesystem.hpp>           // for boost::filesystem
 #include <boost/filesystem/fstream.hpp>   // for ofstream
 
-#include <ElementsKernel/Temporary.h>     // for TempDir
+#include "ElementsKernel/Temporary.h"     // for TempDir, TempEnv
 #include <ElementsKernel/Exception.h>     // for Exception
+#include "ElementsKernel/Path.h"          // for joinPath
+#include "ElementsKernel/System.h"        // for DEFAULT_INSTALL_PREFIX
 
 using std::string;
 using std::vector;
@@ -40,6 +42,9 @@ using boost::filesystem::exists;
 using boost::filesystem::is_regular;
 
 using Elements::TempDir;
+using Elements::TempEnv;
+using Elements::Path::join;
+using Elements::System::DEFAULT_INSTALL_PREFIX;
 
 //-----------------------------------------------------------------------------
 
@@ -53,10 +58,15 @@ struct Configuration_Fixture {
 
   TempDir m_top_dir;
   vector<path> m_item_list;
+  vector<path> m_target_item_list;
+  vector<path> m_real_item_list;
+  vector<path> m_target_real_item_list;
 
   Configuration_Fixture(): m_top_dir{ "Configuration_test-%%%%%%%" } {
 
     using std::for_each;
+    using std::copy_if;
+    using std::distance;
 
     m_item_list.push_back(m_top_dir.path() / "test1");
     m_item_list.push_back(m_top_dir.path() / "test1" / "foo");
@@ -67,6 +77,31 @@ struct Configuration_Fixture {
         [](path p) {
         boost::filesystem::create_directory(p);
     });
+
+    m_item_list.push_back(m_top_dir.path() / "test4");
+
+    m_target_item_list = m_item_list;
+
+    m_target_item_list.push_back(path(DEFAULT_INSTALL_PREFIX) / "share" / "conf");
+
+    m_real_item_list.resize(m_item_list.size());
+    auto it = copy_if(m_item_list.begin(), m_item_list.end(),
+                      m_real_item_list.begin(),
+                      [](const path& p){
+                            return exists(p);
+                      });
+    m_real_item_list.resize(distance(m_real_item_list.begin(), it));
+
+    m_target_real_item_list.resize(m_target_item_list.size());
+    auto it2 = copy_if(m_target_item_list.begin(), m_target_item_list.end(),
+                      m_target_real_item_list.begin(),
+                      [](const path& p){
+                            return exists(p);
+                      });
+    m_target_real_item_list.resize(distance(m_target_real_item_list.begin(), it2));
+
+
+
 
   }
 
@@ -94,8 +129,30 @@ BOOST_AUTO_TEST_CASE(ConfigurationVariableName_test) {
 
 BOOST_FIXTURE_TEST_CASE(getFromLocations_test, Configuration_Fixture) {
 
+  auto env = TempEnv();
+
+  env["ELEMENTS_CONF_PATH"] = join(m_item_list);
+
+  auto locations = Elements::getConfigurationLocations();
+
+  BOOST_CHECK_EQUAL_COLLECTIONS(locations.begin(), locations.end(),
+                                m_target_item_list.begin(), m_target_item_list.end());
 
 }
+
+BOOST_FIXTURE_TEST_CASE(getFromLocationsExist_test, Configuration_Fixture) {
+
+  auto env = TempEnv();
+
+  env["ELEMENTS_CONF_PATH"] = join(m_real_item_list);
+
+  auto locations = Elements::getConfigurationLocations(true);
+
+  BOOST_CHECK_EQUAL_COLLECTIONS(locations.begin(), locations.end(),
+                                m_target_real_item_list.begin(), m_target_real_item_list.end());
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
