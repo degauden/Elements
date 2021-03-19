45d05c0331ea078a49a7f54bc2f4ad48ca6c81a2
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2021-03-19 16:44:53 +0100

    Fix typo

--- cmake/ElementsLocations.cmake
+++ cmake/ElementsLocations.cmake
@@ -179,7 +179,7 @@ endif()
 
 get_arch_lib_dir(that_arch)
 
-message(STATUS "--------------------------------------------------------------------------------------------------> ${CMAKE_PRFIX_PATH}")
+message(STATUS "--------------------------------------------------------------------------------------------------> ${CMAKE_PREFIX_PATH}")
 file(TO_CMAKE_PATH "$ENV{CMAKE_PREFIX_PATH}" current_cmake_prefix_path)
 
 set(ELEMENTS_DEFAULT_SEARCH_PATH)
