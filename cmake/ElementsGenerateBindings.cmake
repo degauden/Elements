9c98aad2c0206d3bb956dc892d753cb8486dac2b
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2021-12-15 16:55:34 +0100

    split the generating function into 2 separate ones for python and C++

--- cmake/ElementsGenerateBindings.cmake
+++ cmake/ElementsGenerateBindings.cmake
@@ -13,26 +13,60 @@ if(generate_pybindings_cmd)
   set(generate_pybindings_cmd ${PYTHON_EXECUTABLE} ${generate_pybindings_cmd})
 endif()
 
-set(cpp_bindings_dir ${CMAKE_CURRENT_BINARY_DIR}/cpp_bindings)
-set(generate_flag ${CMAKE_CURRENT_BINARY_DIR}/generate_flag)
-set(bindings_source_ready ${CMAKE_CURRENT_BINARY_DIR}/bindings_source_ready)
 
-set(py_bindings_dir ${CMAKE_BINARY_DIR}/python)
-
-function(generate_bindings xsd_dir ns_prefix)
+function(get_xsd_files xsd_dir xsd_files)
 
   message(STATUS "Parsing XSD directory : ${xsd_dir}")
   message(STATUS "FROM : ${CMAKE_CURRENT_SOURCE_DIR}")
   #list the XSD files
 
-  file(GLOB_RECURSE xsd_files
+  file(GLOB_RECURSE xsd_files_tmp
        RELATIVE ${xsd_dir}
        ${xsd_dir}/*.xsd
        ${xsd_dir}/*.XSD)
- 
+       
+   set(${xsd_files} ${xsd_files_tmp} PARENT_SCOPE)
+   
+endfunction()
 
+
+function(generate_python_bindings xsd_dir ns_prefix)
+
+  get_xsd_files(${xsd_dir} xsd_files)
+
+  # get the pyxbgen executable
+  find_package(PyXB)
+  if(NOT PYXB_FOUND)
+    message(FATAL_ERROR "Cannot find the PyXB generator executable")
+  endif()
+  
+  set(xsd_files_full ${xsd_files})
+  list(TRANSFORM xsd_files_full PREPEND ${xsd_dir}/)
+
+  set(py_bindings_dir ${CMAKE_BINARY_DIR}/python)
+
+  add_custom_target(PyBindDir ALL ${generate_pybindings_cmd} -o ${py_bindings_dir} -d ${xsd_dir} -m ${package} -n ${ns_prefix}
+                    DEPENDS ${xsd_files_full})
+
+  install(DIRECTORY ${py_bindings_dir}/
+          DESTINATION "python"
+          FILES_MATCHING
+          PATTERN "*"
+          PATTERN "*.pyc" EXCLUDE
+          PATTERN "CVS" EXCLUDE
+          PATTERN ".svn" EXCLUDE)
+
+endfunction()
+
+
+function(generate_cpp_bindings xsd_dir ns_prefix)
+
+  get_xsd_files(${xsd_dir} xsd_files)
+ 
   # get the cpp files (generated from XSD)
   set(xsd_generated_cpp_files ${xsd_files})
+  set(cpp_bindings_dir ${CMAKE_CURRENT_BINARY_DIR}/cpp_bindings)
+
   list(TRANSFORM xsd_generated_cpp_files PREPEND ${cpp_bindings_dir}/src/${package}/)
   list(TRANSFORM xsd_generated_cpp_files REPLACE "\\.xsd$" ".cpp")
   list(TRANSFORM xsd_generated_cpp_files REPLACE "\\.XSD$" ".cpp")
@@ -86,26 +120,12 @@ function(generate_bindings xsd_dir ns_prefix)
   set_property(GLOBAL APPEND PROPERTY REGULAR_INCLUDE_OBJECTS ${package})
 
 
-  # get the pyxbgen executable
-  find_package(PyXB)
-  if(NOT PYXB_FOUND)
-    message(FATAL_ERROR "Cannot find the PyXB generator executable")
-  endif()
-  
-  set(xsd_files_full ${xsd_files})
-  list(TRANSFORM xsd_files_full PREPEND ${xsd_dir}/)
+endfunction()
 
-  add_custom_target(PyBindDir ALL ${generate_pybindings_cmd} -o ${py_bindings_dir} -d ${xsd_dir} -m ${package} -n ${ns_prefix}
-                    DEPENDS ${xsd_files_full})
+function(generate_bindings xsd_dir ns_prefix)
 
-  install(DIRECTORY ${py_bindings_dir}/
-          DESTINATION "python"
-          FILES_MATCHING
-          PATTERN "*"
-          PATTERN "*.pyc" EXCLUDE
-          PATTERN "CVS" EXCLUDE
-          PATTERN ".svn" EXCLUDE)
-
+  generate_cpp_bindings(${xsd_dir} ${ns_prefix})
+  generate_python_bindings(${xsd_dir} ${ns_prefix})
 
 endfunction()
 
