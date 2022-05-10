2002a0824fd01d30804d780fb493545d7553eb40
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2022-05-10 11:54:02 +0200

    Reimplement FinxXsd.cmake to be less verbose
    - use a cached variable for the verbosity
    - defines a target

--- cmake/modules/FindXsd.cmake
+++ cmake/modules/FindXsd.cmake
@@ -1,24 +1,48 @@
-if (NOT XSD_FOUND)
+# -*- cmake -*-
+# - Locate Xsd library
+# Defines:
+#
+#  XSD_FOUND
+#  XSD_INCLUDE_DIR
+#  XSD_INCLUDE_DIRS (not cached)
+#
+# Imports:
+#
+#  Xsd::xsd
+#
 
-    find_program(XSDCXX_EXECUTABLE
-                 NAMES xsdcxx xsd
-                 HINTS ENV XSD_ROOT_DIR XSD_INSTALL_DIR
-                 PATH_SUFFIXES bin)
-    message(STATUS "This is XSDCXX executable: ${XSDCXX_EXECUTABLE}")
+# Find quietly if already found before
+if(DEFINED CACHE{XSD_INCLUDE_DIR})
+  set(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY YES)
+endif()
 
-    find_path(XSD_INCLUDE_DIR xsd/cxx/config.hxx
-              HINTS ENV XSD_ROOT_DIR XSD_INSTALL_DIR
-              PATH_SUFFIXES include)
+find_program(XSDCXX_EXECUTABLE
+             NAMES xsdcxx xsd
+             HINTS ENV XSD_ROOT_DIR XSD_INSTALL_DIR
+             PATH_SUFFIXES bin)
 
-    set(XSD_INCLUDE_DIRS ${XSD_INCLUDE_DIR})
+find_path(XSD_INCLUDE_DIR xsd/cxx/config.hxx
+          HINTS ENV XSD_ROOT_DIR XSD_INSTALL_DIR
+          PATH_SUFFIXES include)
+
 
 # handle the QUIETLY and REQUIRED arguments and set XSD_FOUND to TRUE if
 # all listed variables are TRUE
-    INCLUDE(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Xsd DEFAULT_MSG XSDCXX_EXECUTABLE XSD_INCLUDE_DIRS)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Xsd DEFAULT_MSG XSDCXX_EXECUTABLE XSD_INCLUDE_DIR)
+mark_as_advanced(XSD_FOUND XSDCXX_EXECUTABLE XSD_INCLUDE_DIR)
 
-    mark_as_advanced(XSD_FOUND XSDCXX_EXECUTABLE XSD_INCLUDE_DIRS)
+set(XSD_INCLUDE_DIRS ${XSD_INCLUDE_DIR})
 
-
-endif (NOT XSD_FOUND)
-
+# Modernisation: create an interface target to link against
+if(TARGET Xsd::xsd)
+    return()
+endif()
+if(XSD_FOUND)
+  add_library(Xsd::xsd IMPORTED INTERFACE)
+  target_include_directories(Xsd::xsd SYSTEM INTERFACE "${XSD_INCLUDE_DIRS}")
+  # Display the imported target for the user to know
+  if(NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+    message(STATUS "  Import target: Xsd::xsd")
+  endif()
+endif()
