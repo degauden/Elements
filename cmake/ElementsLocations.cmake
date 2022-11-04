57b5423958d8b781086c1fe0ac517635f0bd33d6
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2022-11-04 17:10:46 +0100

    use the plain "include" name for the install target suffix of .h files

--- cmake/ElementsLocations.cmake
+++ cmake/ElementsLocations.cmake
@@ -112,7 +112,7 @@ set(AUX_DIR_NAME "auxdir" CACHE STRING "Name of the auxiliary files directory")
 set(MAKE_DIR_NAME "make" CACHE STRING "Name of the make files directory")
 set(DOC_DIR_NAME "doc" CACHE STRING "Name of the documentation directory")
 
-set(INCLUDE_INSTALL_SUFFIX ${CMAKE_INSTALL_INCLUDEDIR} CACHE STRING "Final suffix for the install directory of the header files")
+set(INCLUDE_INSTALL_SUFFIX include CACHE STRING "Final suffix for the install directory of the header files")
 set(BIN_INSTALL_SUFFIX ${CMAKE_BIN_INSTALL_SUFFIX} CACHE STRING "Final suffix for the install directory of the binaries")
 set(LIB_INSTALL_SUFFIX ${CMAKE_LIB_INSTALL_SUFFIX} CACHE STRING "Final suffix for the install directory of the libraries")
 
